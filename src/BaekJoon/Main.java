ee22f8d95f22bd11dcfcc21b6a3ba8cde7770de6
ffolabear <kth6699@naver.com>
2020-12-20 23:26:42 +0900

    백준 단계별 문제풀기 진행중 (While) - EOF

--- src/BaekJoon/Main.java
+++ src/BaekJoon/Main.java
@@ -1,22 +1,10 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		String line = br.readLine();
-		int A = Integer.parseInt(br.readLine());
-		
+
+	public static void main(String[] args) {
 		
 	}
+	
 }
---
+++ src/BaekJoon/baek_EOF_scanner.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class baek_EOF_scanner {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		while(in.hasNext()) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			System.out.println(a+b);
+		}
+		in.close();
+
+	}
+}
