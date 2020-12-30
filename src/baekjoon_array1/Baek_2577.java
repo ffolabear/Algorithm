3c6befc968617ea7506aa6b832bf7d0c2f6315b7
ffolabear <kth6699@naver.com>
2020-12-31 02:09:21 +0900

    Programmers Level 1 푸는중

--- src/baekjoon_array1/Baek_2577.java
+++ src/baekjoon_array1/Baek_2577.java
@@ -11,30 +11,28 @@ public class Baek_2577 {
 	public static void main(String[] args) throws Exception, IOException{
 
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		
 		int[] arr = new int[10];
 		int res = Integer.parseInt(bf.readLine()) * Integer.parseInt(bf.readLine()) * Integer.parseInt(bf.readLine());
 		
 		//Integer.toString(res); 도 가능하지만 valueOf는 어떤값을 넣어도 String으로 변환이 가능하다.
 		String str = String.valueOf(res);
-		System.out.println(res);
+		System.out.println(str);
 		
 		
 		for (int i = 0; i < str.length(); i++) {
-			int count = 0;
 			
-			for(int j=0; j< arr.length ; j ++) {
-				if((str.charAt(j)-48) == i) {
-					count++;
-				}
-				System.out.println(count);
-			}
-			
+			arr[(str.charAt(i) - 48)]++;
+		}
+ 
+		for (int v : arr) {
+			System.out.println(v);
 		}
  
 		
 	}
 
 }
+
+
+
---
+++ src/baekjoon_array1/Baek_3052.java
@@ -0,0 +1,43 @@
+package baekjoon_array1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_3052 {
+
+	public static void main(String[] args) throws Exception, IOException {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 입력한 수의 나머지를 저장하는배열
+		// 나머지가 1
+		int[] num = new int[10];
+
+		int[] rem = new int[42];
+
+		for (int i = 0; i < num.length; i++) {
+
+			num[i] = Integer.parseInt(bf.readLine()) % 42;
+
+		}
+
+		for (int i = 0; i < 10; i++) {
+
+			rem[(num[i])]++;
+
+		}
+
+		int count = 0;
+		for (int i = 0; i < rem.length; i++) {
+
+			if (rem[i] != 0) {
+				count++;
+			}
+
+		}
+		System.out.println(count);
+
+	}
+
+}
