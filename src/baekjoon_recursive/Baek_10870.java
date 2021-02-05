9b45ce6ac4f761649bf13d94cbce932f0b4dfd6d
ffolabear <kth6699@naver.com>
2021-02-05 23:50:45 +0900

    알고리즘 공부중 (DoIt, Baek)

---
+++ src/baekjoon_recursive/Baek_10870.java
@@ -0,0 +1,33 @@
+package baekjoon_recursive;
+
+import java.util.Scanner;
+
+public class Baek_10870 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		
+		System.out.println(Fibonacci(N));
+		
+	}
+	
+	
+	static int Fibonacci(int N) {
+		
+		
+		if(N == 0) {
+			return 0;
+		}
+		
+		if(N == 1) {
+			return 1;
+		}
+		
+		return Fibonacci(N-1) + Fibonacci(N-2);
+		
+	}
+
+}
