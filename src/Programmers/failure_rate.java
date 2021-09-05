35e81b7089309240f9953ceab872aa123bdf6e64
ffbear <kth6699@naver.com>
2021-09-06 02:40:23 +0900

    Programmers 2019 카카오 실패율 완료

--- src/Programmers/failure_rate.java
+++ src/Programmers/failure_rate.java
@@ -1,24 +1,66 @@
 package Programmers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 public class failure_rate {
 
     public static int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
 
-        ArrayList<Integer> list = new ArrayList<>();
+        int[] answer = new int[N];
+        double[] ratio = new double[N + 1];
+
+
         for (int i : stages) {
-            list.add(i);
+
+            if (i == N + 1) {
+                continue;
+            }
+            ratio[i]++;
         }
 
+        ArrayList<Double> failure = new ArrayList<Double>();
+        double remainPlayer = stages.length;
+        double failedPlayer = 0;
 
-        for (int stage = 1; stage <= stages.length; stage++) {
+        for (int i = 1; i < ratio.length; i++) {
+            failedPlayer = ratio[i];
 
+            if (remainPlayer == 0) {
+                ratio[i] = 0;
 
+            } else{
+                ratio[i] = ratio[i] / remainPlayer;
 
+                remainPlayer = remainPlayer - failedPlayer;
+            }
+
+            failure.add(ratio[i]);
+
+        }
+
+        Collections.sort(failure, Collections.reverseOrder());
+
+        for (double i : failure) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+
+        for (double i : ratio) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < failure.size(); i++) {
+            for (int j = 1; j < ratio.length; j++) {
+
+                if (failure.get(i) == ratio[j]) {
+                    answer[i] = j;
+                    ratio[j] = -1;
+                    break;
+                }
+
+            }
         }
 
 
