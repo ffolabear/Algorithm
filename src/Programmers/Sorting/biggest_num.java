dad390e23aab53d5e36f1412a8745674e1fd19b5
ffbear <kth6699@naver.com>
2021-09-13 23:50:51 +0900

    Programmers Sort

---
+++ src/Programmers/Sorting/biggest_num.java
@@ -0,0 +1,24 @@
+package Programmers.Sorting;
+
+public class biggest_num {
+
+    public String solution(int[] numbers) {
+
+
+
+
+
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] number = {6, 10, 2};
+//        int[] number = {3, 30, 34, 5, 9};
+
+        biggest_num bnum = new biggest_num();
+        bnum.solution(number);
+    }
+
+}
