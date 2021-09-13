5a960c4d0b8542d12614191be0a24c84a340bcbc
ffbear <kth6699@naver.com>
2021-09-13 23:59:15 +0900

    Programmers Sort

--- src/Programmers/Sorting/biggest_num.java
+++ src/Programmers/Sorting/biggest_num.java
@@ -1,14 +1,43 @@
 package Programmers.Sorting;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class biggest_num {
 
     public String solution(int[] numbers) {
 
+        String[] sortedNum = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            sortedNum[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(sortedNum, new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
 
 
+                return (o2 + o1).compareTo(o1 + o2);
+            }
 
+        });
 
+        for (int i = 0; i < sortedNum.length; i++) {
+            System.out.println(sortedNum[i]);
+        }
         String answer = "";
+
+        if (sortedNum[0] == "0") {
+            answer = "0";
+        } else{
+            for (String s : sortedNum) {
+                answer += s;
+            }
+        }
+
+
         return answer;
     }
 
