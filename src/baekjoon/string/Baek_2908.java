0055af2d7281c109c5d4de1314b70e872d1dae78
ffbear <kth6699@naver.com>
2021-09-19 01:42:21 +0900

    백준 2908 완료

--- src/baekjoon/string/Baek_2908.java
+++ src/baekjoon/string/Baek_2908.java
@@ -10,6 +10,18 @@ public class Baek_2908 {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String str = bf.readLine();
+        StringBuffer buffer;
+
+        String[] nums = str.split(" ");
+
+        for (int i = 0; i < nums.length; i++) {
+
+            buffer = new StringBuffer(nums[i]);
+            nums[i] = buffer.reverse().toString();
+        }
+
+        int answer = Math.max(Integer.parseInt(nums[0]), Integer.parseInt(nums[1]));
+        System.out.println(answer);
 
     }
 }
