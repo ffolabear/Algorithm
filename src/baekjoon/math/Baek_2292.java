05dbf5b96cbc78059e92c449f9bafa31c351f804
ffbear <kth6699@naver.com>
2021-09-21 18:46:18 +0900

    백준 2292 완료

---
+++ src/baekjoon/math/Baek_2292.java
@@ -0,0 +1,42 @@
+package baekjoon.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_2292 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        int start = 1;
+        int answer = 1;
+        int plus = 6;
+
+        if (N == 1) {
+            System.out.println(answer);
+        } else {
+
+            while (true) {
+
+                if (start < N) {
+                    start += plus;
+                    plus += 6;
+                    answer++;
+
+                } else {
+                    System.out.println(answer);
+                    break;
+
+                }
+
+
+            }
+
+        }
+
+
+    }
+
+}
