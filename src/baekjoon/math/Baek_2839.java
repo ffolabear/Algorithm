77b8ce747d8bd67f5058e01396b85c9759b12741
ffbear <kth6699@naver.com>
2021-09-26 23:23:14 +0900

    백준 2839 푸는중

--- src/baekjoon/math/Baek_2839.java
+++ src/baekjoon/math/Baek_2839.java
@@ -10,9 +10,30 @@ public class Baek_2839 {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
+        boolean valid = false;
 
+        int cntThree = 0;
+        int three = N / 3;
+        cntThree += three;
 
+        if (N % 3 == 0) {
+            valid = true;
+        }
 
+        int five = N / 5;
+        int cntFive = 0;
+        cntFive += five;
+
+        if ((N % 5) % 3 == 0) {
+            valid = true;
+            cntFive += 1;
+        }
+
+        System.out.println("3 : " + cntThree);
+        System.out.println("5 : " + cntFive);
+
+        int answer = Math.min(cntFive, cntThree);
+        System.out.println(answer);
 
 
     }
