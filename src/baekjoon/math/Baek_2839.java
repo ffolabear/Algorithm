1f9beb7a7cb0ebd8e16a4062b07281e850928fbe
ffbear <kth6699@naver.com>
2021-09-26 23:07:50 +0900

    백준 2839 푸는중

---
+++ src/baekjoon/math/Baek_2839.java
@@ -0,0 +1,20 @@
+package baekjoon.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_2839 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+
+
+
+
+    }
+
+}
