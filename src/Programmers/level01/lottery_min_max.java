2cf5f316f38692fbd48349c5e35bd166bc1b5c8b
ffbear <kth6699@naver.com>
2021-10-04 08:00:00 +0000

    프로그래머스 lv1 로또의 최고 순위와 최저 순위

---
+++ src/Programmers/level01/lottery_min_max.java
@@ -0,0 +1,56 @@
+package Programmers.level01;
+
+public class lottery_min_max {
+
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+
+        int baseRank = 0;
+        int numOfZero = 0;
+
+        int[] rank = {6, 6, 5, 4, 3, 2, 1};
+
+        for (int i = 0; i < lottos.length; i++) {
+
+            if (lottos[i] == 0) {
+                numOfZero++;
+            }
+
+            for (int j = 0; j < win_nums.length; j++) {
+
+                if (lottos[i] == win_nums[j]) {
+                    win_nums[j] = -1;
+                    baseRank++;
+                }
+            }
+        }
+
+        int sum = numOfZero + baseRank;
+
+
+        answer[0] = rank[sum];
+        answer[1] = rank[baseRank];
+
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] lottos = {44, 1, 0, 0, 31, 25};
+        int[] win_nums = {31, 10, 45, 1, 6, 19};
+
+//        int[] lottos = {0, 0, 0, 0, 0, 0};
+//        int[] win_nums = {38, 19, 20, 40, 15, 25};
+
+//        int[] lottos = {45, 4, 35, 20, 3, 9};
+//        int[] win_nums = {20, 9, 3, 45, 4, 35};
+
+        lottery_min_max sol = new lottery_min_max();
+        sol.solution(lottos, win_nums);
+
+    }
+
+}
