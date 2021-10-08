72600808819a57602a15a88fce7b6acbb809ae37
ffbear <kth6699@naver.com>
2021-10-09 05:03:18 +0900

    프로그래머스 lv2 행렬 테두리 회전하기 완료

--- src/Programmers/level02/EliminationByGrouping.java
+++ src/Programmers/level02/EliminationByGrouping.java
@@ -2,6 +2,7 @@ package Programmers.level02;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class EliminationByGrouping {
 
@@ -11,24 +12,47 @@ public class EliminationByGrouping {
 
         int answer = -1;
         Queue<Character> queue = new LinkedList<>();
-
+        Stack<Character> stack = new Stack<>();
 
 
         for (int i = 0; i < s.length(); i++) {
 
             if (queue.isEmpty()) {
                 queue.offer(s.charAt(i));
+                System.out.println("queue : " + s.charAt(i));
 
             } else{
                 if (queue.peek() == s.charAt(i)) {
+                    System.out.println("같음");
+                    System.out.println("queue : " + queue + " | 제거  - " + queue.peek());
                     queue.poll();
+                }
+                queue.offer(s.charAt(i));
+                System.out.println("queqe : " + queue + " | 추가 : " + s.charAt(i));
+
+            }
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (stack.isEmpty()) {
+                stack.push(s.charAt(i));
+                System.out.println("stack : " + s.charAt(i));
+
+            } else{
+                if (stack.peek() == s.charAt(i)) {
+                    System.out.println("같음");
+                    System.out.println("stack : " + stack + " | 제거  - " + stack.peek());
+                    stack.pop();
                 } else {
-                    queue.offer(s.charAt(i));
+                    stack.push(s.charAt(i));
+                    System.out.println("stack : " + stack + " | 추가 : " + s.charAt(i));
                 }
             }
-
-
         }
+
 
         if (queue.isEmpty()) {
             answer = 1;
