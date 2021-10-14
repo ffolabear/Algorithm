bddb2ebd591971670e92c22906b9495fc790cd02
ffbear <kth6699@naver.com>
2021-10-14 23:57:53 +0900

    프로그래머스 lv2 거리두기 확인하기 - 푸는중

--- src/Programmers/level02/DistanceCheck.java
+++ src/Programmers/level02/DistanceCheck.java
@@ -2,20 +2,78 @@ package Programmers.level02;
 
 public class DistanceCheck {
 
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[] d1x = {-1, 1, 0, 0};
+    static int[] d1y = {0, 0, -1, 1};
+
+    static int[] d2x = {-2, 2, 0, 0};
+    static int[] d2y = {0, 0, -2, 2};
+
+    static int[] gx = {1, 1, -1, -1};
+    static int[] gy = {1, -1, 1, -1};
 
     public int[] solution(String[][] places) {
 
-        boolean[][] visited = new boolean[5][5];
+        int[] answer = new int[places.length];
+
 
         for (int i = 0; i < places.length; i++) {
 
-            boolean valid = true;
-            for (int j = 0; j < places[i].length; j++) {
+            String[] row = places[i];
+            boolean flag = false;
 
-                if (places[i][j].equals("P")) {
-                    check(i, j, places, visited);
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (row[j].charAt(k) == 'P') {
+                        if (check(j, k, row)){
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+                if (flag){
+                    answer[i] = 0;
+                    break;
+                }
+
+            }
+            if (!flag) {
+                answer[i] = 1;
+            }
+
+        }
+
+        return answer;
+    }
+
+    static boolean check(int x, int y, String[] row) {
+
+        //상하좌우 P 탐색
+        for (int i = 0; i < 4; i++) {
+            int nx = x + d1x[i];
+            int ny = y + d1y[i];
+
+            if (nx >= 5 || ny >= 5 || nx < 0 || ny < 0) {
+                continue;
+            }
+
+            if (row[nx].charAt(ny) == 'P') {
+                return true;
+            }
+
+        }
+
+        //대각선 P 탐색
+        for (int i = 0; i < 4; i++) {
+            int nx = x + d2x[i];
+            int ny = y + d2y[i];
+
+            if (nx >= 5 || ny >= 5 || nx < 0 || ny < 0) {
+                continue;
+            }
+
+            if (row[nx].charAt(ny) == 'P') {
+                if (row[(nx + x) / 2].charAt((ny + y) / 2) != 'X') {
+                    return true;
 
                 }
 
@@ -23,36 +81,36 @@ public class DistanceCheck {
 
         }
 
-        int[] answer = {};
-        return answer;
-    }
-
-    static void check(int x, int y, String[][] places, boolean[][] visited) {
-
-        if (visited[x][y]) {
-            return;
-        }
-
-        visited[x][y] = true;
-
+        // 거리2인 상하좌우 P 탐색
         for (int i = 0; i < 4; i++) {
-            int ni = i + dx[i];
-            int ny = i + dy[i];
+            int nx = x + gx[i];
+            int ny = y + gy[i];
 
+            if (nx >= 5 || ny >= 5 || nx < 0 || ny < 0) {
+                continue;
+            }
+
+            if (row[nx].charAt(ny) == 'P') {
+                if (row[x].charAt(ny) == 'X' && row[nx].charAt(y) == 'X') {
+                    return true;
+
+                }
+
+            }
 
         }
 
+        return false;
     }
-
 
 
     public static void main(String[] args) {
 
         String[][] places = {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"},
-                             {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
-                             {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"},
-                             {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},
-                             {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
+                {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
+                {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"},
+                {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},
+                {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
 
         DistanceCheck sol = new DistanceCheck();
         sol.solution(places);
