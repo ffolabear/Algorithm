6697853230e38638d28558a3913b3ceb52e3dd05
ffbear <kth6699@naver.com>
2021-10-23 18:30:02 +0900

    프로그래머스 lv2 소수 찾기 완료

--- src/Programmers/level02/test.java
+++ src/Programmers/level02/test.java
@@ -1,23 +1,72 @@
 package Programmers.level02;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
+class test {
+    ArrayList<Integer> result;
+    boolean[] visited;
 
-public class test {
+    public int solution(String numbers) {
 
-    public static void main(String[] args) {
+        result = new ArrayList<Integer>();
+        visited = new boolean[numbers.length()];
+        int[] nums = new int[numbers.length()];
 
-        String[] str = {"34", "66"};
-        Arrays.sort(str, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                System.out.println(o1 + o2);
-                System.out.println(o2 + o1);
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(numbers.charAt(i)+"");
+        }
 
-                return 0;
+        //자리 수 마다 해당 배열을 perm 메서드에 보낸다.
+        for(int i = 1; i <= numbers.length(); i++) {
+            int[] resultArr = new int[i];
+            perm(nums, resultArr, nums.length, i, 0);
+        }
+
+        return result.size();
+
+    }
+    public boolean isPrime(int num) {
+        if(num <= 1) {
+            return false;
+        }
+        if(num == 2 || num == 3) {
+            return true;
+        }
+
+        int end = (int) Math.sqrt(num);
+        for(int i = 2; i <= end; i++) {
+            if(num % i == 0) {
+                return false;
             }
-        });
+        }
+        return true;
+    }
 
+
+    public void perm(int[] nums, int[] resultArr, int n, int r, int index) {
+        if(index == r) {
+            String realNum = "";
+            for(int i = 0; i < resultArr.length; i++) {
+                realNum += resultArr[i];
+            }
+
+            int num = Integer.parseInt(realNum);
+            if(isPrime(num)) {
+                if(!result.contains(num)) {
+                    result.add(num);
+                }
+            }
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                resultArr[index] = nums[i];
+                perm(nums, resultArr, n, r, index+1);
+                resultArr[index] = 0;
+                visited[i] = false;
+            }
+        }
     }
 
 }
