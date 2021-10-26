d9f397d6450403f9c8b70585c011be3df2af6987
ffbear <kth6699@naver.com>
2021-10-26 23:10:36 +0900

    프로그래머스 lv2 예상 대진표 푸는중

---
+++ src/Programmers/level02/MatchPrediction.java
@@ -0,0 +1,26 @@
+package Programmers.level02;
+
+public class MatchPrediction {
+
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 7;
+        int n = 8;
+
+
+        MatchPrediction sol = new MatchPrediction();
+        sol.solution(n, a, b);
+
+    }
+
+}
