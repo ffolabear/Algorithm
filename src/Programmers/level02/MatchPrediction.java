f6bb41056fa89a5289645b687246b5fe26bd073f
ffbear <kth6699@naver.com>
2021-10-27 14:31:30 +0900

    프로그래머스 lv2 예상 대진표 완료

--- src/Programmers/level02/MatchPrediction.java
+++ src/Programmers/level02/MatchPrediction.java
@@ -1,12 +1,29 @@
 package Programmers.level02;
 
+import java.util.ArrayList;
+
 public class MatchPrediction {
 
     public int solution(int n, int a, int b) {
-        int answer = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        int c = Math.min(a, b);
+        int d = Math.max(a, b);
+
+        int answer = 1;
+
+
+        while (true) {
+
+            if (c % 2 == 1 && d - c == 1) {
+
+                break;
+            }
+
+            c = (c + 1) / 2;
+            d = (d + 1) / 2;
+            answer++;
+
+        }
 
         return answer;
     }
--- src/Programmers/level02/test.java
+++ src/Programmers/level02/test.java
@@ -1,72 +1,12 @@
 package Programmers.level02;
 
-import java.util.*;
-class test {
-    ArrayList<Integer> result;
-    boolean[] visited;
+public class test {
 
-    public int solution(String numbers) {
 
-        result = new ArrayList<Integer>();
-        visited = new boolean[numbers.length()];
-        int[] nums = new int[numbers.length()];
+    public static void main(String[] args) {
 
-        for(int i = 0; i < nums.length; i++) {
-            nums[i] = Integer.parseInt(numbers.charAt(i)+"");
-        }
-
-        //자리 수 마다 해당 배열을 perm 메서드에 보낸다.
-        for(int i = 1; i <= numbers.length(); i++) {
-            int[] resultArr = new int[i];
-            perm(nums, resultArr, nums.length, i, 0);
-        }
-
-        return result.size();
+        System.out.println((4 + 1)/2);
+        System.out.println((5 + 1)/2);
 
     }
-    public boolean isPrime(int num) {
-        if(num <= 1) {
-            return false;
-        }
-        if(num == 2 || num == 3) {
-            return true;
-        }
-
-        int end = (int) Math.sqrt(num);
-        for(int i = 2; i <= end; i++) {
-            if(num % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
-    public void perm(int[] nums, int[] resultArr, int n, int r, int index) {
-        if(index == r) {
-            String realNum = "";
-            for(int i = 0; i < resultArr.length; i++) {
-                realNum += resultArr[i];
-            }
-
-            int num = Integer.parseInt(realNum);
-            if(isPrime(num)) {
-                if(!result.contains(num)) {
-                    result.add(num);
-                }
-            }
-            return;
-        }
-
-        for(int i = 0; i < nums.length; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                resultArr[index] = nums[i];
-                perm(nums, resultArr, n, r, index+1);
-                resultArr[index] = 0;
-                visited[i] = false;
-            }
-        }
-    }
-
 }
