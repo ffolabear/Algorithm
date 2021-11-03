0d8019d256e7382679111c86a15cd1d15b689b05
ffbear <kth6699@naver.com>
2021-11-03 20:46:25 +0900

    프로그래머스 lv2 다리를 지나는 트럭 - 풀었음

--- src/Programmers/level02/BridgeCrossingTruck2.java
+++ src/Programmers/level02/BridgeCrossingTruck2.java
@@ -6,57 +6,51 @@ import java.util.Queue;
 public class BridgeCrossingTruck2 {
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
+        int time = 0;
 
         Queue<Integer> queue = new LinkedList<>();
 
-        int time = 0;
+        int sum = 0;
 
         for (int i = 0; i < truck_weights.length; i++) {
+            //트럭이 다리를 지날떄는 3가지 경우
 
+            while (true) {
 
-            //현재 다리위에있는 무게들을 구할 변수
-            int sum = 0;
+                //다리가 비어있을때
+                if (queue.isEmpty()) {
+                    queue.add(truck_weights[i]);
+                    sum += truck_weights[i];
+                    time++;
+                    break;
 
-            if (queue.isEmpty()) {
+                //다리가 트럭으로 꽉차있을때
+                } else if (queue.size() == bridge_length) {
+                    int temp =  queue.poll();
+                    sum -= temp;
 
-                queue.add(truck_weights[i]);
-                sum += truck_weights[i];
-                System.out.println("queue가 비어있음 : " + sum);
-                time++;
+                //아직 다리에 트럭을 있음
+                } else {
 
-            } else {
-                System.out.println(sum);
-                //큐의 크기가 다리의 길이보다 작다 -> 무게조건만 충족하면 트럭을 더 올릴수있다.
-
-                if (queue.size() < bridge_length) {
-
-                    //아직 무게 괜찮음. 트럭 올라갈수있음
-                    if (sum + truck_weights[i] <= weight) {
+                    if (truck_weights[i] + sum <= weight) {
                         queue.add(truck_weights[i]);
                         sum += truck_weights[i];
                         time++;
+                        break;
+
                     } else {
-                        //다른 트럭을 올릴수 없으므로 시간만 지나가므로 0을 더해줌
                         queue.add(0);
-
                         time++;
-
                     }
 
-                } else if (queue.size() == bridge_length) {
-
-                    sum -= queue.poll();
                 }
-                //큐의 사이즈 = 다리의 길이
-
             }
+
 
 
         }
 
-        System.out.println(time);
-        return answer;
+        return time + bridge_length;
     }
 
     public static void main(String[] args) {
