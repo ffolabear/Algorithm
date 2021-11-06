87ede9fe2d55631279a0c1d54d2e00d8155e75b6
ffbear <kth6699@naver.com>
2021-11-06 23:56:08 +0900

    프로그래머스 lv2 피로도 - 푸는중

---
+++ src/Programmers/level02/Fatigue.java
@@ -0,0 +1,14 @@
+package Programmers.level02;
+
+public class Fatigue {
+
+    public int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
