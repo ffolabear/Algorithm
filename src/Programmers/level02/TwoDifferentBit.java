6d1fa06401f136ea77ec40234a48f07898f482b6
ffbear <kth6699@naver.com>
2021-11-10 23:43:46 +0900

    프로그래머스 lv2 2개 이하로 다른 비트 - 푸는중

---
+++ src/Programmers/level02/TwoDifferentBit.java
@@ -0,0 +1,17 @@
+package Programmers.level02;
+
+public class TwoDifferentBit {
+
+    public long[] solution(long[] numbers) {
+        long[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        long[] numbers = {2, 7};
+        TwoDifferentBit sol = new TwoDifferentBit();
+        sol.solution(numbers);
+
+    }
+}
