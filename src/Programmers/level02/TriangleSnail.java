d8a2ed4e311781f627eca4d4b192f26660dbcfa2
ffbear <kth6699@naver.com>
2021-11-11 23:57:01 +0900

    프로그래머스 lv2 2개 이하로 다른 비트, 삼각 달팽이 - 푸는중

--- src/Programmers/level02/TriangleSnail.java
+++ src/Programmers/level02/TriangleSnail.java
@@ -1,4 +1,20 @@
 package Programmers.level02;
 
 public class TriangleSnail {
+
+
+    public int[] solution(int n) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 4;
+        TriangleSnail sol = new TriangleSnail();
+        sol.solution(n);
+
+    }
+
+
 }
