cf2550e46fdf2eeb60c925c6b7494c0ac1464bcf
ffbear <kth6699@naver.com>
2021-11-12 23:58:28 +0900

    프로그래머스 lv2 2개 이하로 다른 삼각 달팽이 - 푸는중

--- src/Programmers/level02/TriangleSnail.java
+++ src/Programmers/level02/TriangleSnail.java
@@ -1,9 +1,26 @@
 package Programmers.level02;
 
+import java.util.ArrayList;
+
 public class TriangleSnail {
 
 
     public int[] solution(int n) {
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            list.add(row);
+        }
+
+        System.out.println(list);
+
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+
+
         int[] answer = {};
         return answer;
     }
