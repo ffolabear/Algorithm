ac5d31da1d04ab4b73f9216a18c3890749e0a5c5
ffbear <kth6699@naver.com>
2021-11-14 13:46:51 +0900

    프로그래머스 lv2 구명 보트 - 푸는중

---
+++ src/Programmers/level02/LifeBoat.java
@@ -0,0 +1,28 @@
+package Programmers.level02;
+
+public class LifeBoat {
+
+    public int solution(int[] people, int limit) {
+
+
+
+
+
+
+
+
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] people = {70, 50, 80, 50};
+        int limit = 100;
+
+        LifeBoat sol = new LifeBoat();
+        sol.solution(people, limit);
+
+    }
+
+}
