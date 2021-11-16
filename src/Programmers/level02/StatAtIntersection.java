a6f5ae7371fbf0457d8b15cf0c13e814570e7b87
ffbear <kth6699@naver.com>
2021-11-16 17:58:39 +0900

    프로그래머스 lv2 - 교점에 별찍기 푸는중

---
+++ src/Programmers/level02/StatAtIntersection.java
@@ -0,0 +1,21 @@
+package Programmers.level02;
+
+public class StatAtIntersection {
+
+    public String[] solution(int[][] line) {
+        String[] answer = {};
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[][] line = {{2, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12}};
+
+        StatAtIntersection sol = new StatAtIntersection();
+        sol.solution(line);
+
+
+    }
+
+}
