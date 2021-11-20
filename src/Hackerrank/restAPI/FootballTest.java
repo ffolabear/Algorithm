46e6d0b163c6951ec95fdcfb9ce0ee70cc78e4f8
ffbear <kth6699@naver.com>
2021-11-20 18:20:03 +0900

    gitignore 테스트

---
+++ src/Hackerrank/restAPI/FootballTest.java
@@ -0,0 +1,18 @@
+package Hackerrank.restAPI;
+
+public class FootballTest {
+
+    public static void main(String[] args) {
+
+        FootballTest test = new FootballTest();
+
+
+    }
+
+    private static final String MATCH_URL = "https://jsonmock.hackerrank.com/api/football_matches";
+    private static final String COMPETITION_URL = "https://jsonmock.hackerrank.com/api/football_competitions";
+
+
+
+
+}
