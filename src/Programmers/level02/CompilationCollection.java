ff0b17da1b55d0ba0f303cd5f868a7546f2570a7
ffbear <kth6699@naver.com>
2021-11-21 23:48:06 +0900

    프로그래머스 lv2 모음사전 - 푸는중

---
+++ src/Programmers/level02/CompilationCollection.java
@@ -0,0 +1,21 @@
+package Programmers.level02;
+
+public class CompilationCollection {
+
+    public int solution(String word) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        String word = "AAAAE";
+
+
+        CompilationCollection sol = new CompilationCollection();
+        sol.solution(word);
+
+    }
+
+
+}
