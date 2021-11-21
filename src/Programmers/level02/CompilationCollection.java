eb46ef573ec062dff557436456befb759f0ab574
ffbear <kth6699@naver.com>
2021-11-21 23:58:23 +0900

    프로그래머스 lv2 모음사전 - 푸는중

--- src/Programmers/level02/CompilationCollection.java
+++ src/Programmers/level02/CompilationCollection.java
@@ -1,8 +1,28 @@
 package Programmers.level02;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CompilationCollection {
 
     public int solution(String word) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 0);
+        map.put('E', 1);
+        map.put('I', 2);
+        map.put('O', 3);
+        map.put('U', 4);
+
+        int order = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            order += map.get(word.charAt(i));
+        }
+
+        System.out.println(order);
+
+
         int answer = 0;
         return answer;
     }
