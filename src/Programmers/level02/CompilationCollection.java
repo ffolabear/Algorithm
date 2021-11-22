aa63a1f21b13f760f4458bad62072db2179a39ef
ffbear <kth6699@naver.com>
2021-11-22 21:05:29 +0900

    프로그래머스 lv2 모음사전 - 풀었음

--- src/Programmers/level02/CompilationCollection.java
+++ src/Programmers/level02/CompilationCollection.java
@@ -1,39 +1,45 @@
 package Programmers.level02;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class CompilationCollection {
 
     public int solution(String word) {
 
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('A', 0);
-        map.put('E', 1);
-        map.put('I', 2);
-        map.put('O', 3);
-        map.put('U', 4);
-
-        int order = 0;
-
-        for (int i = 0; i < word.length(); i++) {
-            order += map.get(word.charAt(i));
-        }
-
-        System.out.println(order);
-
+        int[] digit = {781, 156, 31, 6, 1};
+        char[] alphabet = word.toCharArray();
 
         int answer = 0;
+
+        int cnt = word.length();
+
+        while (cnt > 0) {
+            if (alphabet[word.length() - cnt] == 'A') {
+
+                answer += digit[alphabet.length - cnt] * 0 + 1;
+            } else if (alphabet[word.length() - cnt] == 'E') {
+                answer += digit[alphabet.length - cnt] * 1 + 1;
+            }else if (alphabet[word.length() - cnt] == 'I') {
+                answer += digit[alphabet.length - cnt] * 2 + 1;
+            }else if (alphabet[word.length() - cnt] == 'O') {
+                answer += digit[alphabet.length - cnt] * 3 + 1;
+            }else if (alphabet[word.length() - cnt] == 'U') {
+                answer += digit[alphabet.length - cnt] * 4 + 1;
+            }
+
+            cnt--;
+
+        }
+
         return answer;
     }
 
     public static void main(String[] args) {
 
-        String word = "AAAAE";
+        String word1 = "AAAE";
 
 
         CompilationCollection sol = new CompilationCollection();
-        sol.solution(word);
+        sol.solution(word1);
 
     }
 
