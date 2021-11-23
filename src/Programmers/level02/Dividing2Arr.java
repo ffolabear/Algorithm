c05e14b44aa4f1a03f7ce769cc580b0536ed9a72
ffbear <kth6699@naver.com>
2021-11-23 23:29:23 +0900

    프로그래머스 lv2 n^2 배열 자르기 - 푸는중

---
+++ src/Programmers/level02/Dividing2Arr.java
@@ -0,0 +1,20 @@
+package Programmers.level02;
+
+public class Dividing2Arr {
+
+    public int[] solution(int n, long left, long right) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Dividing2Arr sol = new Dividing2Arr();
+        int n = 3;
+        int left = 2;
+        int right = 5;
+        sol.solution(n, left, right);
+
+    }
+
+}
