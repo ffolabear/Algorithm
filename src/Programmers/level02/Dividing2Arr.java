00f71edf87b5f966f8fe831969b775f4753a5469
ffbear <kth6699@naver.com>
2021-11-24 23:56:38 +0900

    프로그래머스 lv2 n^2 배열 자르기 - 완료

--- src/Programmers/level02/Dividing2Arr.java
+++ src/Programmers/level02/Dividing2Arr.java
@@ -1,50 +1,28 @@
 package Programmers.level02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dividing2Arr {
 
-    public int[] solution(int n, long left, long right) {
+    public List<Long> solution(int n, long left, long right) {
 
-        left += 1;
-        right += 1;
+        List<Long> list = new ArrayList<>();
 
-        int rowLeft = 0;
-        int rowRight = 0;
+        for (long i = left; i <= right; i++) {
 
-        int length = (int) Math.pow(n, 2);
-
-        for (int i = 1; i <= n; i++) {
-            if (left < (long) i * n) {
-                rowLeft = i;
-                break;
-            }
+            list.add(Math.max(i / n, i % n) + 1);
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (right < (long) i * n) {
-                rowRight = i;
-                break;
-            }
-        }
-
-        System.out.println(left);
-        System.out.println(right);
-
-        System.out.println(n * (rowLeft - 1) + " ~ " + n * rowLeft);
-        System.out.println(n * (rowRight - 1) + " ~ " + n * rowRight);
-
-
-        int[] answer = {};
-
-
-        return answer;
+        return list;
     }
 
     public static void main(String[] args) {
 
         Dividing2Arr sol = new Dividing2Arr();
-        int n = 7;
+        int n =7;
         int left = 14;
-        int right = 28;
+        int right =29;
         sol.solution(n, left, right);
 
     }
