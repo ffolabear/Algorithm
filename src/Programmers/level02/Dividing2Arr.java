4ddf3b9d27676624198b2811dad96532d857862b
ffbear <kth6699@naver.com>
2021-11-24 20:33:54 +0900

    프로그래머스 lv2 n^2 배열 자르기 - 테스트케이스 통과했으나 오답

--- src/Programmers/level02/Dividing2Arr.java
+++ src/Programmers/level02/Dividing2Arr.java
@@ -4,16 +4,45 @@ public class Dividing2Arr {
 
     public int[] solution(int n, long left, long right) {
 
-        int[][] arr = new int[n][n];
+        StringBuilder sb = new StringBuilder();
 
-        int[] answer = {};
+        long index = 1;
+        int count = 0;
+
+        while (index <= n) {
+
+            for (long i = 1; i <= n; i++) {
+                if (count >= left && count <= right) {
+                    if (i <= index) {
+                        sb.append(index);
+                    } else {
+                        sb.append(i);
+                    }
+
+                } else {
+                    count++;
+                    continue;
+                }
+                count++;
+            }
+            index++;
+        }
+
+        System.out.println(sb);
+
+        int[] answer = new int[sb.length()];
+
+        for (int i = 0; i < sb.length(); i++) {
+            answer[i] = sb.charAt(i) - '0';
+        }
+
         return answer;
     }
 
     public static void main(String[] args) {
 
         Dividing2Arr sol = new Dividing2Arr();
-        int n = 3;
+        int n = 7;
         int left = 2;
         int right = 5;
         sol.solution(n, left, right);
