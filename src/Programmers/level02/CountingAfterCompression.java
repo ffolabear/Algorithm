2e33f8979385be1ca775e4a0f25526cd962b631d
ffbear <kth6699@naver.com>
2021-11-26 23:59:07 +0900

    프로그래머스 lv2 쿼드압축 후 개수 세기 - 푸는중

--- src/Programmers/level02/CountingAfterCompression.java
+++ src/Programmers/level02/CountingAfterCompression.java
@@ -3,13 +3,27 @@ package Programmers.level02;
 public class CountingAfterCompression {
 
     public int[] solution(int[][] arr) {
+
+        int length = arr.length;
+
+        while (length > 1) {
+            compression(length /= 2, arr);
+        }
+
         int[] answer = {};
         return answer;
     }
 
+    static void compression(int length, int[][] arr) {
+        System.out.println("Called");
+    }
+
     public static void main(String[] args) {
 
-        int[][] arr = {{1,1,0,0}, {1,0,0,0}, {1,0,0,1}, {1,1,1,1}};
+        int[][] arr = {{1, 1, 0, 0},
+                       {1, 0, 0, 0},
+                       {1, 0, 0, 1},
+                       {1, 1, 1, 1}};
 
         CountingAfterCompression sol = new CountingAfterCompression();
         sol.solution(arr);
