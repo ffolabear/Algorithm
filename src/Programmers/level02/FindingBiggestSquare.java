0ea873fc84df04123c51252097718fee90c7853b
ffbear <kth6699@naver.com>
2021-11-28 02:33:10 +0900

    프로그래머스 lv2 가장 큰 정사각형 찾기  - 푸는중

---
+++ src/Programmers/level02/FindingBiggestSquare.java
@@ -0,0 +1,23 @@
+package Programmers.level02;
+
+public class FindingBiggestSquare {
+
+
+
+    public int solution(int[][] board) {
+        int answer = 1234;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] board = {{0, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {0, 0, 1, 0}};
+        FindingBiggestSquare sol = new FindingBiggestSquare();
+        sol.solution(board);
+
+    }
+}
