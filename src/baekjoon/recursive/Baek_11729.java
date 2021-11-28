0abdcd98e7e762547d9204115b4fda176a1fa5fd
ffbear <kth6699@naver.com>
2021-11-28 23:59:02 +0900

    백준 하노이의 탑 푸는중

---
+++ src/baekjoon/recursive/Baek_11729.java
@@ -0,0 +1,43 @@
+package baekjoon.recursive;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_11729 {
+
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        recursion(N, 1, 3);
+
+        System.out.println(count);
+
+
+    }
+
+    static void recursion(int N, int from, int to) {
+
+        System.out.println(from + " " + to);
+        count++;
+
+        if (N > 1) {
+            recursion(N - 1, from,6 - from - to);
+        }
+
+        System.out.println(from + " " + to);
+
+        if (N > 1) {
+            recursion(N - 1, 6 - from - to, to);
+        }
+
+
+
+    }
+
+
+
+}
