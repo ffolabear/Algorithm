630ed081b50481358ca3a8936d8c037ac62b2c43
ffbear <kth6699@naver.com>
2021-11-28 13:03:10 +0900

    프로그래머스 lv2 가장 큰 정사각형 찾기  - 푸는중

--- src/Programmers/level02/FindingBiggestSquare.java
+++ src/Programmers/level02/FindingBiggestSquare.java
@@ -3,19 +3,53 @@ package Programmers.level02;
 public class FindingBiggestSquare {
 
 
-
     public int solution(int[][] board) {
-        int answer = 1234;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        int max = 0;
+
+        int[][] newBoard = newBoard = new int[board.length + 1][board[0].length + 1];
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                newBoard[i + 1][j + 1] = board[i][j];
+            }
+        }
+
+
+        for (int i = 1; i < newBoard.length; i++) {
+            for (int j = 1; j < newBoard[i].length; j++) {
+                if (newBoard[i][j] == 1) {
+
+                    int left = newBoard[i - 1][j];
+                    int up = newBoard[i][j - 1];
+                    int leftUp = newBoard[i - 1][j - 1];
+
+                    //각 꼭지점의 값 체크했을때 꼭짓점이 0인경우가 있을수있음
+                    int min = Math.min(left, Math.min(up, leftUp));
+
+                    //꼭짓점이 0이면 정사각형이 아니므로 1로 초기화
+                    //0이 아니라면 길이가 1...2...3 식으로 바꿔서 저장
+                    newBoard[i][j] = min + 1;
+                    max = Math.max(max, min + 1);
+
+                }
+            }
+        }
+
+        int answer = max * max;
+        System.out.println(answer);
 
         return answer;
     }
 
     public static void main(String[] args) {
 
-        int[][] board = {{0, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {0, 0, 1, 0}};
+        int[][] board = {
+                {0, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {0, 0, 1, 0}};
+
         FindingBiggestSquare sol = new FindingBiggestSquare();
         sol.solution(board);
 
