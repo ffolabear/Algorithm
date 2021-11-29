489f9ae8f78ff56b79c217047dd6ffe743db2d92
ffbear <kth6699@naver.com>
2021-11-30 03:10:03 +0900

    프로그래머스 lv2 [3차] 압축 - 푸는중

--- src/Programmers/level02/Compression.java
+++ src/Programmers/level02/Compression.java
@@ -1,26 +1,42 @@
 package Programmers.level02;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Compression {
 
-    public int[] solution(String msg) {
+    public ArrayList<Integer> solution(String msg) {
 
-        HashMap<Character, Integer> dict = new HashMap<>();
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<String, Integer> dict = new HashMap<>();
         int idx = 1;
 
         for (int i = 65; i <= 90; i++) {
+            dict.put(String.valueOf((char) i), idx);
+            idx++;
+        }
 
+        String temp = "";
+
+        for (int i = 0; i < msg.length(); i++) {
+            temp += String.valueOf(msg.charAt(i));
+
+            if (!dict.containsKey(temp)) {
+                dict.put(temp, idx);
+                idx++;
+                temp = String.valueOf(msg.charAt(i));
+            }
+            answer.add(dict.get(temp));
         }
 
 
-        int[] answer = {};
         return answer;
     }
 
     public static void main(String[] args) {
         Compression sol = new Compression();
         String msg = "KAKAO";
+//        String msg = "TOBEORNOTTOBEORTOBEORNOT";
         sol.solution(msg);
     }
 }
