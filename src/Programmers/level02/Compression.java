3d431dfc937cb3057d41c52aba0a384603a7134d
ffbear <kth6699@naver.com>
2021-11-29 23:32:57 +0900

    프로그래머스 lv2 [3차] 압축 - 푸는중

---
+++ src/Programmers/level02/Compression.java
@@ -0,0 +1,15 @@
+package Programmers.level02;
+
+public class Compression {
+
+    public int[] solution(String msg) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Compression sol = new Compression();
+        String msg = "KAKAO";
+        sol.solution(msg);
+    }
+}
