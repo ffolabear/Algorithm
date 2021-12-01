cf32037033174d82a8ac7d81d7d78bc181884ad1
ffbear <kth6699@naver.com>
2021-12-01 17:38:47 +0900

    프로그래머스 lv2 [3차] 파일명 정렬 - 푸는중, 문자열 나누기

--- src/Programmers/level02/SortingFileName.java
+++ src/Programmers/level02/SortingFileName.java
@@ -1,5 +1,7 @@
 package Programmers.level02;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Locale;
 
 public class SortingFileName {
@@ -12,32 +14,57 @@ public class SortingFileName {
         for (int i = 0; i < files.length; i++) {
             files[i] = files[i].toLowerCase(Locale.ROOT);
 
+            int headIdx = 0;
+
             for (int j = 0; j < files[i].length(); j++) {
                 if (files[i].charAt(j) >= 48 && files[i].charAt(j) <= 57) {
                     divide[i][0] = files[i].substring(0, j);
-                    System.out.println("HEAD : " + divide[i][0]);
+                    headIdx = j;
 
-                    System.out.println(files[i].charAt(j));
-                    for (int k = j; k < files[i].length(); k++) {
+                    int numIdx = 0;
 
-                        if (files[i].charAt(k) < 48 && files[i].charAt(k) > 57) {
+                    for (int k = headIdx; k < files[i].length(); k++) {
 
-                            divide[i][1] = files[i].substring(j, k);
-                            System.out.println("NUMBER : " + divide[i][1]);
+                        if (String.valueOf(files[i].charAt(k)).matches("[0-9]")) {
+                            numIdx = k;
+                        } else {
+                            divide[i][1] = files[i].substring(headIdx, numIdx += 1);
                             break;
+
                         }
+
                     }
+
+                    divide[i][2] = files[i].substring(numIdx + 1);
+
+                    break;
+
                 }
-
-
-
 
 
             }
 
+
+        }
+
+        for (int i = 0; i < divide.length; i++) {
+            for (int j = 0; j < divide[i].length; j++) {
+
+                System.out.print(divide[i][j] + " ");
+
+            }
+            System.out.println();
         }
 
 
+        Arrays.sort(files, new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                return 0;
+            }
+
+        });
 
 
         String[] answer = {};
@@ -46,25 +73,25 @@ public class SortingFileName {
 
     public static void main(String[] args) {
 
-//        String[] files = {
-//
-//                "img12.png",
-//                "img10.png",
-//                "img02.png",
-//                "img1.png",
-//                "IMG01.GIF",
-//                "img2.JPG"
-//
-//        };
-
         String[] files = {
 
-                "F-5 Freedom Fighter",
-                "B-50 Superfortress",
-                "A-10 Thunderbolt II",
-                "F-14 Tomcat"
+                "img12.png",
+                "img10.png",
+                "img02.png",
+                "img1.png",
+                "IMG01.GIF",
+                "img2.JPG"
 
         };
+
+//        String[] files = {
+//
+//                "F-5 Freedom Fighter",
+//                "B-50 Superfortress",
+//                "A-10 Thunderbolt II",
+//                "F-14 Tomcat"
+//
+//        };
 
 
         // 출력 예졔
