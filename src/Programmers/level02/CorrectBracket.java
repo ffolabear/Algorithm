f6c0443884e286deedc400a98d1fa0ed7754bba1
ffbear <kth6699@naver.com>
2021-12-02 23:52:56 +0900

    프로그래머스 lv2 올바른 괄호 - 푸는중

--- src/Programmers/level02/CorrectBracket.java
+++ src/Programmers/level02/CorrectBracket.java
@@ -1,11 +1,20 @@
 package Programmers.level02;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CorrectBracket {
     boolean solution(String s) {
         boolean answer = true;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+
+        }
+
+        System.out.println(queue);
 
         return answer;
     }
