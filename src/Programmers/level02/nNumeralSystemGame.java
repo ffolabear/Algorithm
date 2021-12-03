9472d5ba2864b0f5c428bed40d3134a3eac26474
ffbear <kth6699@naver.com>
2021-12-03 22:41:23 +0900

    프로그래머스 lv2 [3차] n진수 게임 - 푸는중

---
+++ src/Programmers/level02/nNumeralSystemGame.java
@@ -0,0 +1,22 @@
+package Programmers.level02;
+
+public class nNumeralSystemGame {
+
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        int t = 4;
+        int m = 2;
+        int p = 1;
+
+        nNumeralSystemGame sol = new nNumeralSystemGame();
+        sol.solution(n, t, m, p);
+
+
+    }
+
+}
