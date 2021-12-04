1a17989f453394887d43b20e9355f5fa80dabfe1
ffbear <kth6699@naver.com>
2021-12-04 21:45:40 +0900

    프로그래머스 lv2 [3차] n진수 게임 - 푸는중

--- src/Programmers/level02/nNumeralSystemGame.java
+++ src/Programmers/level02/nNumeralSystemGame.java
@@ -4,7 +4,51 @@ public class nNumeralSystemGame {
 
     public String solution(int n, int t, int m, int p) {
         String answer = "";
+
+
+        int numCnt = 0;
+        int number = 0;
+
+        while (numCnt != t) {
+
+
+
+
+        }
+
         return answer;
+    }
+
+    static String convert(int n, int num) {
+
+        System.out.println("현재 숫자 : " + num);
+
+        String ans = "";
+
+
+        for (int i = num; i > 0; i /= n) {
+
+            if (num % n == 10) {
+                ans = "A" + ans;
+            } else if (num % n == 11) {
+                ans = "B" + ans;
+            } else if (num % n == 12) {
+                ans = "C" + ans;
+            } else if (num % n == 13) {
+                ans = "D" + ans;
+            } else if (num % n == 14) {
+                ans = "E" + ans;
+            } else if (num % n == 15) {
+                ans = "F" + ans;
+            } else {
+                ans = (num % n) + ans;
+
+            }
+            num /= n;
+        }
+
+        System.out.println(ans);
+        return ans;
     }
 
     public static void main(String[] args) {
