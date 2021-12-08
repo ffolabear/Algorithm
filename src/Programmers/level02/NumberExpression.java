cde3b89a9dbc6483fcfb12f4a7bbba62596e3396
ffbear <kth6699@naver.com>
2021-12-08 23:55:05 +0900

    프로그래머스 lv2 숫자의 표현 - 푸는중

---
+++ src/Programmers/level02/NumberExpression.java
@@ -0,0 +1,18 @@
+package Programmers.level02;
+
+public class NumberExpression {
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 15;
+        NumberExpression sol = new NumberExpression();
+        sol.solution(n);
+
+
+    }
+}
