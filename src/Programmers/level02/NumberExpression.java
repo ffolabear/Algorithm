0a00d1ca815de867d86812a3186fccf16ca2747a
ffbear <kth6699@naver.com>
2021-12-09 23:58:30 +0900

    프로그래머스 lv2 숫자의 표현 - 푸는중

--- src/Programmers/level02/NumberExpression.java
+++ src/Programmers/level02/NumberExpression.java
@@ -3,9 +3,30 @@ package Programmers.level02;
 public class NumberExpression {
 
     public int solution(int n) {
+
         int answer = 0;
+
+        int cnt = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            if (cnt == n) {
+                break;
+            }
+
+            cnt += i;
+
+
+        }
+
+
+
+
+
         return answer;
     }
+
+
 
     public static void main(String[] args) {
 
