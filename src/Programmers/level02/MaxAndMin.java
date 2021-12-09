df705375f01ca3c3bb27babd704026deaa2af2e5
ffbear <kth6699@naver.com>
2021-12-10 00:18:07 +0900

    프로그래머스 lv2 최댓값과 최솟값 - 풀었음

--- src/Programmers/level02/MaxAndMin.java
+++ src/Programmers/level02/MaxAndMin.java
@@ -16,11 +16,18 @@ public class MaxAndMin {
             index++;
         }
 
-        for (int i : nums) {
-            System.out.println(i);
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
         }
 
-        String answer = "";
+
+
+        String answer = min + " " + max;
+        System.out.println(answer);
         return answer;
     }
 
