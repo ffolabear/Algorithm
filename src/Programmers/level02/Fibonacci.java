2fdc6df50e544c6539eb16be44bb383e72838e03
ffbear <kth6699@naver.com>
2021-12-11 00:16:06 +0900

    프로그래머스 lv2 피보나치수 - 푸는중(재귀 시간초과)

--- src/Programmers/level02/Fibonacci.java
+++ src/Programmers/level02/Fibonacci.java
@@ -3,13 +3,29 @@ package Programmers.level02;
 public class Fibonacci {
 
     public int solution(int n) {
-        int answer = 0;
+
+        int answer = Fibo(n) & 1234567;
+
         return answer;
+    }
+
+    static int Fibo(int n) {
+
+        System.out.println(n);
+
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+
+        return Fibo(n - 1) + Fibo(n - 2);
+
     }
 
     public static void main(String[] args) {
 
-        int n = 3;
+        int n = 8;
         Fibonacci sol = new Fibonacci();
         sol.solution(n);
 
