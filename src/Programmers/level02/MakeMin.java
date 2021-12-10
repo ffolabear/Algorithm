9985c859e4e9e21f3af01670695a89a9ac8d1013
ffbear <kth6699@naver.com>
2021-12-10 16:57:54 +0900

    프로그래머스 lv2 최솟값 만들기 - 푸는중

---
+++ src/Programmers/level02/MakeMin.java
@@ -0,0 +1,24 @@
+package Programmers.level02;
+
+public class MakeMin {
+
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] A = {1, 4, 2};
+        int[] B = {5, 4, 4};
+
+        MakeMin sol = new MakeMin();
+        sol.solution(A, B);
+
+    }
+
+}
