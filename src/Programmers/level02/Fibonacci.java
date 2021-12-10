691672fbe54b5146bf92250016165a162033c0de
ffbear <kth6699@naver.com>
2021-12-10 17:27:41 +0900

    프로그래머스 lv2 피보나치수 - 푸는중

---
+++ src/Programmers/level02/Fibonacci.java
@@ -0,0 +1,17 @@
+package Programmers.level02;
+
+public class Fibonacci {
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 3;
+        Fibonacci sol = new Fibonacci();
+        sol.solution(n);
+
+    }
+}
