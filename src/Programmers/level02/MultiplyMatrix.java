dbc23530fbd413f8dfaf5836c8618eea2da5b48b
ffbear <kth6699@naver.com>
2021-12-13 05:33:28 +0900

    프로그래머스 lv2 행렬의 곱셈 - 풀었음

--- src/Programmers/level02/MultiplyMatrix.java
+++ src/Programmers/level02/MultiplyMatrix.java
@@ -4,7 +4,20 @@ public class MultiplyMatrix {
 
     public int[][] solution(int[][] arr1, int[][] arr2) {
 
-        int[][] answer = {};
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+
+                }
+                System.out.println(answer[i][j]);
+
+            }
+        }
+
 
         return answer;
     }
@@ -12,9 +25,7 @@ public class MultiplyMatrix {
     public static void main(String[] args) {
 
         int[][] arr1 = {
-                {1, 4},
-                {3, 2},
-                {4, 1}
+                {1, 4}, {3, 2}, {4, 1}
         };
 
         int[][] arr2 = {
