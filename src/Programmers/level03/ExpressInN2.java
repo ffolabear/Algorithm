f0a57ffaf58ad2b97f66ae6e100957263cc695fe
ffbear <kth6699@naver.com>
2021-12-16 23:57:13 +0900

    프로그래머스 lv3 N으로 표현 - 푸는중

---
+++ src/Programmers/level03/ExpressInN2.java
@@ -0,0 +1,20 @@
+package Programmers.level03;
+
+public class ExpressInN2 {
+
+    public int solution(int N, int number) {
+
+        int answer = -1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int N = 5;
+        int number = 12;
+        ExpressInN2 sol = new ExpressInN2();
+        sol.solution(N, number);
+
+
+    }
+}
