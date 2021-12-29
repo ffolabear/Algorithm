12fa1d46038023f9e04f17de6f48b0806ce47ff4
ffbear <kth6699@naver.com>
2021-12-30 06:11:19 +0900

    백준 코딩테스트 강의 문제 리스트 - Boj 16929 Two Dots 푸는중

--- src/baekjoon/lecture/graph/Boj16929.java
+++ src/baekjoon/lecture/graph/Boj16929.java
@@ -8,11 +8,12 @@ import java.util.StringTokenizer;
 public class Boj16929 {
 
     static int N, M;
+    static int initX, initY;
     static String[][] board;
     static boolean[][] visited;
 
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
 
 
     public static void main(String[] args) throws IOException {
@@ -24,21 +25,57 @@ public class Boj16929 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        board = new String[N + 1][M + 1];
-        visited = new boolean[N + 1][M + 1];
+        board = new String[N][M];
 
-        for (int i = 0; i < board.length; i++) {
+
+        for (int i = 0; i < N; i++) {
             board[i] = br.readLine().split("");
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
 
+                visited = new boolean[N][M];
 
+                initX = i;
+                initY = j;
 
+                if (dfs(i, j, 1)) {
+                    System.out.println("Yes");
+                    return;
+                }
             }
         }
 
+        System.out.println("No");
+
+    }
+
+    static boolean dfs(int x, int y, int count) {
+
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < M && board[x][y].equals(board[nx][ny])) {
+                if (!visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    if (dfs(nx, ny, count + 1)) {
+                        return true;
+                    }
+
+                } else {
+                    if (count >= 4 && initX == nx && initY == ny) {
+                        return true;
+                    }
+                }
+            }
+
+        }
+
+        return false;
     }
 
 
