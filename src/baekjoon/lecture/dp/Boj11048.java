73c1754b6e3ff3ba54b664df39718692efeefc86
ffbear <kth6699@naver.com>
2022-01-01 23:56:08 +0900

    백준 코딩테스트 강의 문제 리스트 - Boj 11048 이동하기 푸는중

--- src/baekjoon/lecture/dp/Boj11048.java
+++ src/baekjoon/lecture/dp/Boj11048.java
@@ -16,18 +16,21 @@ public class Boj11048 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] maze = new int[N][M];
+        int[][] maze = new int[N + 1][M + 1];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i < maze.length; i++) {
             String[] row = br.readLine().split(" ");
-            for (int j = 0; j < M; j++) {
-                maze[i][j] = Integer.parseInt(row[j]);
+            for (int j = 1; j < maze[i].length; j++) {
+                maze[i][j] = Integer.parseInt(row[j - 1]);
             }
 
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+
+
+
+        for (int i = 1; i < maze.length; i++) {
+            for (int j = 1; j < maze[i].length; j++) {
                 System.out.print(maze[i][j] + " ");
             }
             System.out.println();
