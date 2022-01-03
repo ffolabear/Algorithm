a63c3f8dce316354d71aae2a651b2d3e7d4b98ab
ffbear <kth6699@naver.com>
2022-01-03 19:31:32 +0900

    백준 코딩테스트 강의 문제 리스트 - Boj 16234 인구 이동 푸는중

--- src/baekjoon/lecture/simulation/Boj16234.java
+++ src/baekjoon/lecture/simulation/Boj16234.java
@@ -7,18 +7,91 @@ import java.util.StringTokenizer;
 
 public class Boj16234 {
 
+    static int N, L, R;
+
+    static int[][] country;
+    static boolean[][] visited;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
         StringTokenizer st = new StringTokenizer(input);
 
-        int N = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
-        int R = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
 
         System.out.println(N + " " + L + " " + R);
+        System.out.println("----------------------");
+
+        country = new int[N][N];
+        visited = new boolean[N][N];
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+
+            String[] row = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                country[i][j] = Integer.parseInt(row[j]);
+            }
+
+        }
+
+        for (int i = 0; i < country.length; i++) {
+            for (int j = 0; j < country[i].length; j++) {
+
+                if (checkMove(i, j)) {
+                    answer++;
+                }
+
+            }
+        }
+
+        //-----------------------------------------------------------------------------------
+
+        for (int i = 0; i < country.length; i++) {
+            for (int j = 0; j < country[i].length; j++) {
+                System.out.print(country[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        System.out.println(answer);
+    }
+
+    static boolean checkMove(int x, int y) {
+
+        visited[x][y] = true;
+
+        System.out.println(x + " | " + y);
+        System.out.println("------------");
+
+        for (int i = 0; i < 4; i++) {
+            int nx = dx[i];
+            int ny = dy[i];
+
+
+            if (nx >= 0 && ny >= 0 &&
+                nx < N && ny < N) {
+                System.out.println(nx + " | " + ny + " = " + i);
+            }
+
+        }
+        System.out.println();
+        return false;
+    }
+
+    static void movePeople(int x, int y, int dx, int dy) {
+        country[x][y] = (country[x][y] + country[dx][dy]) / 2;
+        country[dx][dy] = (country[x][y] + country[dx][dy]) / 2;
 
     }
+
 
 }
