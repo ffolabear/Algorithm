3dd60f330d82d4101e2bb2222bfa37b7ddaf2084
ffbear <kth6699@naver.com>
2022-01-13 02:03:44 +0900

    백준 코딩테스트 강의 문제 리스트 - Boj 16940번 연산자 끼워넣기

--- src/baekjoon/lecture/graph/Boj16940.java
+++ src/baekjoon/lecture/graph/Boj16940.java
@@ -3,70 +3,89 @@ package baekjoon.lecture.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Boj16940 {
 
     static int N;
-    static ArrayList<ArrayList<Integer>> adj;
-    static int[] visit;
-    static boolean[] visited;
+    static ArrayList<Integer>[] adj;
+    static Queue<Integer> order = new LinkedList<>();
+
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        adj = new ArrayList<>();
+        adj = new ArrayList[N + 1];
 
 
-        for (int i = 1; i < N; i++) {
-            adj.add(new ArrayList<>());
+        for (int i = 1; i <= N; i++) {
+            adj[i] = new ArrayList<>();
         }
 
 
-        for (int i = 1; i < N; i++) {
+        for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int parent = Integer.parseInt(st.nextToken());
-            int child = Integer.parseInt(st.nextToken());
-            adj.get(parent).add(child);
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+
+            adj[from].add(to);
+            adj[to].add(from);
 
         }
-
-        System.out.println(adj);
 
         String[] row = br.readLine().split(" ");
-        visit = new int[row.length];
-
         for (int i = 0; i < row.length; i++) {
-            visit[i] = Integer.parseInt(row[i]);
+            order.add(Integer.parseInt(row[i]));
+        }
 
+        if(order.poll()!=1) {
+            System.out.println(0);
+            return;
+        }
+
+        if (bfs()) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
         }
 
 
     }
 
-    static boolean check() {
+    static boolean bfs() {
 
-        Queue<Integer> parentQueue = new LinkedList<>();
-        int parent = 1;
-        for (int i = 1; i < N; i++) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[N + 1];
+        visited[1] = true;
+        queue.add(1);
 
-            int current = visit[i];
+        while(!queue.isEmpty()) {
+            int temp = queue.poll();
+            HashSet<Integer> set = new HashSet<>();
 
-            if (adj.get(current).size() > 1) {
-                parentQueue.add(current);
+            for(int next : adj[temp]) {
+                if(visited[next]) continue;
 
-
+                visited[next] = true;
+                set.add(next);
             }
 
+            if(set.size()==0) continue;
 
+            for(int i=0; i<set.size(); i++) {
+                int next = order.poll();
+
+                if(set.contains(next))
+                    queue.add(next);
+
+                else
+                    return false;
+            }
         }
 
-
-        return false;
+        return true;
     }
 
 
