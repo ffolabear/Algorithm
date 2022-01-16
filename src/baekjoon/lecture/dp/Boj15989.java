c0be14ea2e8bf7c97f081aee7e94ba1db8edb351
ffbear <kth6699@naver.com>
2022-01-17 02:20:12 +0900

    백준 코딩테스트 강의 문제 리스트 - Boj 15898번 1, 2, 3 더하기 4

--- src/baekjoon/lecture/dp/Boj15989.java
+++ src/baekjoon/lecture/dp/Boj15989.java
@@ -1,34 +1,44 @@
 package baekjoon.lecture.dp;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Boj15989 {
 
-    static int[] nums;
+    static int[][] dp;
 
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
-        nums = new int[T];
+        dp = new int[100001][4];
 
-        for (int i = 0; i < T; i++) {
-            nums[i] = Integer.parseInt(br.readLine());
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
 
-            System.out.println(nums[i]);
+        for (int i = 4; i <= 10000; i++) {
+            dp[i][1] = dp[i - 1][1];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
         }
 
+        for (int i = 0; i < T; i++) {
+
+            int input = Integer.parseInt(br.readLine());
+            bw.write(dp[input][1] + dp[input][2] + dp[input][3] + "\n");
+
+        }
+
+        bw.flush();
+        bw.close();
 
     }
 
-    static int dp(int nums) {
-
-
-        return 1;
-    }
 
 }
