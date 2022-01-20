8ccd0876bceaad07d0ec12fe4b06727e7068dc7c
ffbear <kth6699@naver.com>
2022-01-21 03:11:24 +0900

    백준 코딩테스트 강의 문제 리스트 - Boj 15658 연산자 끼워넣기(2)

--- src/baekjoon/lecture/bruteforce/recursive/Boj15658.java
+++ src/baekjoon/lecture/bruteforce/recursive/Boj15658.java
@@ -3,23 +3,43 @@ package baekjoon.lecture.bruteforce.recursive;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Boj15658 {
+
+    static int N;
+    static int[] operators = new int[4];
+    static int[] nums;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
+        //숫자의 갯수
+        N = Integer.parseInt(br.readLine());
+        System.out.println("N : " + N);
+
+        //입력받은 숫자
         String[] numbers = br.readLine().split(" ");
-        int[] nums = new int[numbers.length];
+        nums = new int[numbers.length];
+
 
 
         for (int i = 0; i < numbers.length; i++) {
             nums[i] = Integer.parseInt(numbers[i]);
-
-
+            System.out.print(nums[i] + " ");
         }
+        System.out.println();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        operators[0] = Integer.parseInt(st.nextToken());
+        operators[1] = Integer.parseInt(st.nextToken());
+        operators[2] = Integer.parseInt(st.nextToken());
+        operators[3] = Integer.parseInt(st.nextToken());
+
+        System.out.println(operators[0] + " " + operators[1] + " " + operators[2] + " " + operators[3]);
+
 
 
     }
