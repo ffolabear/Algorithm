244ef338b45522994f18a7f57f49f39c3e4ed246
ffbear <kth6699@naver.com>
2022-01-24 04:45:00 +0900

    백준 코딩테스트 실버 - Boj 1002 터렛

--- src/baekjoon/silver/Boj1002.java
+++ src/baekjoon/silver/Boj1002.java
@@ -23,11 +23,57 @@ public class Boj1002 {
                 location[i][j] = Integer.parseInt(input[j]);
             }
         }
+
+        for (int i = 0; i < location.length; i++) {
+
+            System.out.println(calculation(location[i]));
+        }
+
     }
 
     static int calculation(int[] testcase) {
 
-        return 0;
+
+        int x1 = testcase[0];
+        int y1 = testcase[1];
+        int r1 = testcase[2];
+
+        int x2 = testcase[3];
+        int y2 = testcase[4];
+        int r2 = testcase[5];
+
+        int distance = (int)(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+
+
+
+        //완전히 일치
+        if (x1 == x2 && y1 == y2 && r1 == r2) {
+
+            return -1;
+
+        //원안에 원이 있지만 내접 X
+        } else if (distance < Math.pow(r1 - r2, 2)) {
+            return 0;
+
+        //내접
+        } else if (distance == Math.pow(r1 - r2, 2)) {
+            return 1;
+
+        //외접
+        } else if (distance == Math.pow(r1 + r2, 2)) {
+            return 1;
+
+        //반지름의 합보다 두 점 사이의 거리가 길때
+        } else if (distance > Math.pow(r1 + r2, 2)) {
+            return 0;
+
+        } else {
+
+            return 2;
+        }
+
+
     }
 
 }
