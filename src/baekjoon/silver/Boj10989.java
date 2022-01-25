351f6f505f753c0bbaec1c1eeb5ca201f8f23d2f
ffbear <kth6699@naver.com>
2022-01-25 23:57:24 +0900

    백준 코딩테스트 실버 - Boj 10989 수 정렬하기 3 푸는중

---
+++ src/baekjoon/silver/Boj10989.java
@@ -0,0 +1,30 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Boj10989 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(nums);
+
+        for (int i : nums) {
+            System.out.println(i);
+        }
+
+
+    }
+
+}
