c53af222702924b31871da82614f56da369d8df0
ffbear <kth6699@naver.com>
2022-01-26 23:58:44 +0900

    백준 코딩테스트 실버 - Boj 10815 숫자 카드 풀었음

--- src/baekjoon/silver/Boj10815.java
+++ src/baekjoon/silver/Boj10815.java
@@ -1,6 +1,8 @@
 package baekjoon.silver;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Boj10815 {
 
@@ -10,6 +12,35 @@ public class Boj10815 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
+        HashMap<Integer, Boolean> nums = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while (st.hasMoreTokens()) {
+            int current = Integer.parseInt(st.nextToken());
+
+            nums.put(current, true);
+
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+
+        while (st.hasMoreTokens()) {
+            int current = Integer.parseInt(st.nextToken());
+
+            if (nums.containsKey(current)) {
+                bw.write(1 + " ");
+            } else {
+                bw.write(0 + " ");
+
+            }
+        }
+
+
+        bw.flush();
+        bw.close();
 
     }
 }
