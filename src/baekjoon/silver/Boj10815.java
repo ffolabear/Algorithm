7d88955ff164bf7a281863a0c1f2e2cfb573b939
ffbear <kth6699@naver.com>
2022-01-26 23:29:54 +0900

    백준 코딩테스트 실버 - Boj 10815 숫자 카드 푸는중

---
+++ src/baekjoon/silver/Boj10815.java
@@ -0,0 +1,15 @@
+package baekjoon.silver;
+
+import java.io.*;
+
+public class Boj10815 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+    }
+}
