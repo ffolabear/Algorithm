09e05f874ee820dad1513ff06b1c07e13ac0569b
ffbear <kth6699@naver.com>
2022-01-27 17:28:19 +0900

    백준 코딩테스트 실버 - Boj 10845 큐 푸는중

--- src/baekjoon/silver/Boj10845.java
+++ src/baekjoon/silver/Boj10845.java
@@ -1,60 +1,79 @@
 package baekjoon.silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class Boj10845 {
 
-    static List<Integer> stack = new ArrayList<>();
+    static List<Integer> queue = new ArrayList<>();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
             String command = br.readLine();
 
             if (command.matches("push.*")) {
-                System.out.println("1");
-            }else if (command.matches("pop")) {
-                System.out.println("2");
-            }else if (command.matches("size")) {
-                System.out.println("3");
-            }else if (command.matches("empty")) {
-                System.out.println("4");            }else if (command.matches("front")) {
-                System.out.println("5");
-            }else if (command.matches("back")) {
-                System.out.println("6");
+                String[] input = command.split(" ");
+                push(Integer.parseInt(input[1]));
+
+            } else if (command.matches("pop")) {
+                bw.write(pop() + "\n");
+
+            } else if (command.matches("size")) {
+                bw.write(size() + "\n");
+
+            } else if (command.matches("empty")) {
+                bw.write(empty() + "\n");
+
+            } else if (command.matches("front")) {
+                bw.write(front() +  "\n");
+
+            } else if (command.matches("back")) {
+                bw.write(back() + "\n");
+
             }
 
         }
 
+        bw.flush();
+        bw.close();
 
     }
 
 
     static void push(int element) {
-
-
+        queue.add(element);
     }
 
-    static void pop() {
+    static int pop() throws IOException {
+
+        if (queue.size() == 0) {
+            return -1;
+
+        } else {
+
+            int index = queue.get(0);
+            queue.remove(0);
+            return index;
+        }
+
 
     }
 
     static int size() {
 
-        return stack.size();
+        return queue.size();
     }
 
     static int empty() {
 
-        if (stack.size() == 0) {
+        if (queue.size() == 0) {
             return 1;
         } else {
             return 0;
@@ -64,19 +83,19 @@ public class Boj10845 {
 
     static int front() {
 
-        if (stack.size() == 0) {
+        if (queue.size() == 0) {
             return -1;
         } else {
 
-            return stack.get(0);
+            return queue.get(0);
         }
     }
 
     static int back() {
-        if (stack.size() == 0) {
+        if (queue.size() == 0) {
             return -1;
         } else {
-            return stack.get(stack.size() - 1);
+            return queue.get(queue.size() - 1);
         }
 
     }
