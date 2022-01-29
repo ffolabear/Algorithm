2f7dfbf2707802dd3435641bc5f23d18452cf91b
ffbear <kth6699@naver.com>
2022-01-29 19:32:14 +0900

    백준 코딩테스트 실버 - Boj 1026 보물

---
+++ src/baekjoon/silver/Boj1026.java
@@ -0,0 +1,46 @@
+package baekjoon.silver;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Boj1026 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        String[] inputA = br.readLine().split(" ");
+        Integer[] A = new Integer[inputA.length];
+
+        for (int i = 0; i < A.length; i++) {
+            A[i] = Integer.parseInt(inputA[i]);
+        }
+
+        String[] inputB = br.readLine().split(" ");
+        Integer[] B = new Integer[inputB.length];
+        for (int i = 0; i < B.length; i++) {
+            B[i] = Integer.parseInt(inputB[i]);
+        }
+
+
+        Arrays.sort(A);
+        Arrays.sort(B, Collections.reverseOrder());
+        int answer = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[i];
+        }
+
+
+        bw.write(String.valueOf(answer));
+
+        bw.flush();
+        bw.close();
+
+
+    }
+
+}
