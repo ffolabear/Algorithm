fb135d9f8ea84c23fc73542e17e199d2afb2d4f4
ffbear <kth6699@naver.com>
2022-01-31 00:02:17 +0900

    백준 코딩테스트 실버 - Boj 11650 좌표 정렬하기

--- src/baekjoon/silver/Boj11650_sortingCoordinate.java
+++ src/baekjoon/silver/Boj11650_sortingCoordinate.java
@@ -28,7 +28,7 @@ public class Boj11650_sortingCoordinate {
             @Override
             public int compare(int[] o1, int[] o2) {
 
-                if (o1[0] == o2[1]) {
+                if (o1[0] == o2[0]) {
                     return o1[1] - o2[1];
                 } else {
 
