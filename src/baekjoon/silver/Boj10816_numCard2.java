2b7e4c44f73cb77bb6075e0e5fcd366865735160
ffbear <kth6699@naver.com>
2022-02-01 23:55:31 +0900

    백준 코딩테스트 실버 - Boj 10816 숫자 카드 2 정렬하기

--- src/baekjoon/silver/Boj10816_numCard2.java
+++ src/baekjoon/silver/Boj10816_numCard2.java
@@ -1,40 +1,1 @@
-package baekjoon.silver;
-
-import java.io.*;
-import java.util.StringTokenizer;
-
-public class Boj10816_numCard2 {
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-
-        String[] input1 = br.readLine().split(" ");
-
-        int[] arr1 = new int[N];
-
-        for (int i = 0; i < arr1.length; i++) {
-
-            arr1[i] = Integer.parseInt(input1[i]);
-
-        }
-
-        int M = Integer.parseInt(br.readLine());
-
-        String[] input2 = br.readLine().split(" ");
-
-        int[] arr2 = new int[N];
-
-        for (int i = 0; i < arr2.length; i++) {
-
-            arr2[i] = Integer.parseInt(input1[i]);
-
-        }
-
-
-    }
-
-}
+package baekjoon.silver;import java.io.*;import java.util.HashMap;import java.util.StringTokenizer;public class Boj10816_numCard2 {    public static void main(String[] args) throws IOException {        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));        int N = Integer.parseInt(br.readLine());        HashMap<Integer, Integer> map = new HashMap<>();        StringTokenizer st = new StringTokenizer(br.readLine());        while (st.hasMoreTokens()) {            int current = Integer.parseInt(st.nextToken());            if (map.containsKey(current)) {                map.put(current, map.get(current) + 1);            } else {                map.put(current, 1);            }        }        System.out.println(map);        st = new StringTokenizer(br.readLine());        while (st.hasMoreTokens()) {            int current = Integer.parseInt(st.nextToken());            bw.write(map.get(current) + " ");        }        bw.flush();        bw.close();    }}
