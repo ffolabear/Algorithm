2dbaf5dd8f6519f4f619ce2d5280333aaf573a2b
ffbear <kth6699@naver.com>
2022-02-04 23:58:37 +0900

    백준 코딩테스트 실버 - Boj 1158 요새푸스 수열

---
+++ src/baekjoon/silver/Boj1158_josephus.java
@@ -0,0 +1,20 @@
+package baekjoon.silver;
+
+import java.io.*;
+
+public class Boj1158_josephus {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        boolean[] isBanished = new boolean[N + 1];
+
+
+
+
+    }
+
+}
