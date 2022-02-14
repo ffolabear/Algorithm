495143167d129c4530c48fb19bca40c0b5bc2849
ffbear <kth6699@naver.com>
2022-02-14 16:20:51 +0900

    sw 아카데미 - 보물상자 문제 수정중

--- src/swexpert/sw_5658.java
+++ src/swexpert/sw_5658.java
@@ -1,35 +1,51 @@
 package swexpert;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class sw_5658 {
 
+    static Scanner sc = new Scanner(System.in);
+    static int T;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String args[]) throws Exception {
 
 
-        Scanner sc = new Scanner(System.in);
-        int T;
         T = sc.nextInt();
+
+        for (int i = 1; i <= T; i++) {
+
+            calculation(i);
+
+        }
+        System.out.println(sb);
+
+    }
+
+    static void calculation(int testcase) {
 
         int N = sc.nextInt();
         int K = sc.nextInt();
+
         String input = sc.next();
 
         int side = N / 4;
 
         ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> arr = new ArrayList<>();
 
-        for (int test_case = 1; test_case <= T; test_case++) {
+        for (int i = 1; i <= 4; i++) {
             int index = 0;
 
             while (index < N) {
                 String fragment = input.substring(index, index += side);
-
-                list.add(Integer.parseInt(fragment, 16));
+                int convert = Integer.parseInt(fragment, 16);
+                list.add(convert);
             }
 
-
-
+            System.out.println(input);
 
             String remain = input.substring(0, input.length() - 1);
             String end = input.substring(input.length() - 1, input.length());
@@ -37,19 +53,27 @@ public class sw_5658 {
 
         }
 
-        Collections.sort(list, Collections.reverseOrder());
+        System.out.println();
+        list.sort(Collections.reverseOrder());
+        System.out.println(list);
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int prev = -1;
+        int count = 0;
+
 
         for (int i = 0; i < list.size(); i++) {
-            if (map.containsKey(list.get(i))) {
-                continue;
+            if (prev != list.get(i)) {
+                count++;
             } else {
-                map.put(list.get(i), i);
+                continue;
             }
-        }
 
-        System.out.println(map);
+            if (count == K) {
+                sb.append("#").append(testcase).append(" ").append(list.get(i)).append("\n");
+            }
+
+            prev = list.get(i);
+        }
 
     }
 
