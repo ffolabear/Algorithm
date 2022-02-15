6487ead6d512ffbb364d5aab09e2c331e0fe741d
ffbear <kth6699@naver.com>
2022-02-16 05:03:55 +0900

    백준 실버 - Boj 1966 프린터 큐

--- src/baekjoon/silver/Boj1966_printerQueue.java
+++ src/baekjoon/silver/Boj1966_printerQueue.java
@@ -1,8 +1,10 @@
 package baekjoon.silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Boj1966_printerQueue {
@@ -10,6 +12,7 @@ public class Boj1966_printerQueue {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int testcase = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < testcase; i++) {
@@ -17,12 +20,59 @@ public class Boj1966_printerQueue {
 
             //문서의 갯수
             int N = Integer.parseInt(st.nextToken());
-            //궁금한 문서가 현재 Queue에서 몇 번째에 놓여 있는지를 나타내는
+            //궁금한 문서가 현재 Queue 에서 몇 번째에 놓여 있는지를 나타내는
             int M = Integer.parseInt(st.nextToken());
 
+            System.out.println(N + " " + M);
+
+            LinkedList<int[]> printer = new LinkedList<>();
+
+            //중요도
+            StringTokenizer nums = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                printer.add(new int[]{i, Integer.parseInt(nums.nextToken())});
+            }
+
+            int count = 0;
+
+            while (!printer.isEmpty()) {
+
+                int[] first = printer.poll();
+                boolean isBiggest = true;
+
+                for (int k = 0; k < printer.size(); k++) {
+
+                    if (first[1] < printer.get(k)[1]) {
+                        printer.offer(first);
+
+                        for (int l = 0; l < k; l++) {
+                            printer.offer(printer.poll());
+                        }
+
+                        isBiggest = false;
+                        break;
+                    }
+                }
+
+                if (!isBiggest) {
+                    continue;
+                }
+
+                count++;
+                if (first[0] == M) {
+                    break;
+
+                }
+            }
+
+            bw.append(String.valueOf(count)).append("\n");
 
         }
 
-    }
+        bw.flush();
+        bw.close();
 
+
+    }
 }
