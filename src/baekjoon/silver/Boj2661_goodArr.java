261ecaadef370cb1b9f5dbb4636bdbacdb362235
ffbear <kth6699@naver.com>
2022-02-18 12:55:50 +0900

    백준 벡트래킹 - Boj 15649 N과M(1) (Boj 2661 좋은 수열 풀기위한 벡트래킹 공부)

--- src/baekjoon/silver/Boj2661_goodArr.java
+++ src/baekjoon/silver/Boj2661_goodArr.java
@@ -1,7 +1,13 @@
 package baekjoon.silver;
 
+import java.io.*;
+
 public class Boj2661_goodArr {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int testcase = Integer.parseInt(br.readLine());
 
     }
 
