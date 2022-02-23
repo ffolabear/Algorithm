f3f9309a86d1a27eb38d06017de54b18775d5bf8
ffbear <kth6699@naver.com>
2022-02-23 23:58:39 +0900

    백준 깊이 우선 탐색 - Boj 2606 바이러스

---
+++ src/baekjoon/dfs/Boj_2606.java
@@ -0,0 +1,21 @@
+package baekjoon.dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_2606 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+        }
+
+
+    }
+
+}
