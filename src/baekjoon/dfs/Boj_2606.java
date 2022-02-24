875c7114654fa357ffea60d59f8e920e1e0866b5
ffbear <kth6699@naver.com>
2022-02-24 23:33:53 +0900

    백준 깊이 우선 탐색 - Boj 2606 바이러스

--- src/baekjoon/dfs/Boj_2606.java
+++ src/baekjoon/dfs/Boj_2606.java
@@ -3,6 +3,8 @@ package baekjoon.dfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Boj_2606 {
 
@@ -11,8 +13,26 @@ public class Boj_2606 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            String input = br.readLine();
+        ArrayList<Integer>[] node = new ArrayList[N+1];
+
+        for (int i = 0; i <= N; i++) {
+            node[i] = new ArrayList<>();
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            node[from].add(to);
+            node[to].add(from);
+
+        }
+
+        for (ArrayList<Integer> arr : node) {
+            System.out.println(arr);
         }
 
 
