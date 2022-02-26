2750a9f62f18ac3f048edbb9fc09193014b7a625
ffbear <kth6699@naver.com>
2022-02-26 19:32:28 +0900

    백준 깊이 우선 탐색 - Boj 2606 바이러스

--- src/baekjoon/dfs/Boj_2606.java
+++ src/baekjoon/dfs/Boj_2606.java
@@ -8,18 +8,23 @@ import java.util.StringTokenizer;
 
 public class Boj_2606 {
 
+    static int N, M;
+    static ArrayList<Integer>[] node;
+    static boolean[] visited;
+    static int answer = 0;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer>[] node = new ArrayList[N+1];
+        node = new ArrayList[N + 1];
 
         for (int i = 0; i <= N; i++) {
             node[i] = new ArrayList<>();
         }
 
-        int M = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < M; i++) {
 
@@ -31,8 +36,22 @@ public class Boj_2606 {
 
         }
 
-        for (ArrayList<Integer> arr : node) {
-            System.out.println(arr);
+
+        visited = new boolean[N + 1];
+        dfs(1);
+        System.out.println(answer);
+    }
+
+    static void dfs(int current) {
+
+        visited[current] = true;
+
+        for (int i : node[current]) {
+            if (!visited[i]) {
+                visited[current] = true;
+                answer += 1;
+                dfs(i);
+            }
         }
 
 
