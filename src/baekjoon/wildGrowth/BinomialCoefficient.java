5ae7048d046edcf9e203c8ad314ebeaf0637d239
ffolabear <kth6699@naver.com>
2022-03-09 23:09:32 +0900

    백준 - Boj11401 이항 계수 3 푸는중

---
+++ src/baekjoon/wildGrowth/BinomialCoefficient.java
@@ -0,0 +1,19 @@
+package baekjoon.wildGrowth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BinomialCoefficient {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+    }
+
+}
