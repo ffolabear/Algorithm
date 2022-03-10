c844360139364c4ac8970cd2f785fcbe8679c4ea
ffolabear <kth6699@naver.com>
2022-03-11 00:56:47 +0900

    백준 - Boj11051 이항 계수 2

--- src/baekjoon/wildGrowth/BinomialCoefficient2.java
+++ src/baekjoon/wildGrowth/BinomialCoefficient2.java
@@ -1,42 +1,33 @@
 package baekjoon.wildGrowth;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class BinomialCoefficient2 {
-
-    /*
-        모듈러 연산
-        (a + b) % m = ((a % m) + (b % m)) % m
-        (a * b) % m = ((a % m) * (b % m)) % m
-     */
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        int[][] triangle = new int[1001][1001];
 
-    }
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= i; j++) {
 
-    static int factorial(int n) {
-
-        int res = 1;
-
-        for (int i = 1; i <= n; i++) {
-            res *= i;
+                if (i == j || j == 0) {
+                    triangle[i][j] = 1;
+                } else {
+                    triangle[i][j] = (triangle[i - 1][j - 1] + triangle[i - 1][j]) % 10007;
+                }
+            }
         }
 
-        return res;
-    }
+        System.out.println(triangle[N][K]);
 
-    static int inverse(int a, int p) {
-
-        return 0;
     }
 
 }
