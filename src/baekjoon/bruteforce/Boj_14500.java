80afed904f2bd9b299035f3caa2fd1eac47038c8
ffolabear <kth6699@naver.com>
2022-03-12 23:56:53 +0900

    백준 - Boj 14500 테트로미노 푸는중

--- src/baekjoon/bruteforce/Boj_14500.java
+++ src/baekjoon/bruteforce/Boj_14500.java
@@ -1,6 +1,46 @@
 package baekjoon.bruteforce;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Boj_14500 {
+
+    static int N, M, ans;
+    static int[][] board;
+
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String[] row = br.readLine().split(" ");
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(row[j]);
+            }
+        }
+
+
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 
 
 }
