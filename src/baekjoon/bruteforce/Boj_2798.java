324b422f80d3555ac0f6f7346ac3a76c3326be63
ffolabear <kth6699@naver.com>
2022-03-16 00:12:27 +0900

    백준 Brute force - 2798 블랙잭

--- src/baekjoon/bruteforce/Boj_2798.java
+++ src/baekjoon/bruteforce/Boj_2798.java
@@ -7,26 +7,49 @@ import java.util.StringTokenizer;
 
 public class Boj_2798 {
 
+    static int N, M;
+    static int[] nums;
+    static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         String[] input = br.readLine().split(" ");
-        int[] nums = new int[input.length];
+
+        nums = new int[input.length];
+        visited = new boolean[nums.length];
 
         for (int i = 0; i < input.length; i++) {
             nums[i] = Integer.parseInt(input[i]);
         }
 
+        System.out.println(search());
+
+    }
+
+    static int search() {
+
+        int res = 0;
         int sum = 0;
 
-        while (sum <= M) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    sum = nums[i] + nums[j] + nums[k];
+                    if (sum >= res && sum <= M) {
+                        res = sum;
+                    }
+                }
+            }
 
         }
 
+        return res;
     }
 
 }
