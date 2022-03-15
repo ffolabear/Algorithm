ad25e5357607678edd81f3d3938ab5dfc7521ffb
ffolabear <kth6699@naver.com>
2022-03-15 23:58:36 +0900

    백준 Brute force - 2798 블랙잭 푸는중

---
+++ src/baekjoon/bruteforce/Boj_2798.java
@@ -0,0 +1,32 @@
+package baekjoon.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_2798 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        String[] input = br.readLine().split(" ");
+        int[] nums = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            nums[i] = Integer.parseInt(input[i]);
+        }
+
+        int sum = 0;
+
+        while (sum <= M) {
+
+        }
+
+    }
+
+}
