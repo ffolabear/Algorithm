aee3487720f4682ddd475065bfe6a037a58755d4
ffolabear <kth6699@naver.com>
2022-03-20 23:57:28 +0900

    백준 Brute force - 16198 에너지 모으기 푸는중

---
+++ src/baekjoon/bruteforce/Boj_16198.java
@@ -0,0 +1,27 @@
+package baekjoon.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_16198 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[N];
+
+        int index = 0;
+        while (st.hasMoreTokens()) {
+            nums[index++] = Integer.parseInt(st.nextToken());
+        }
+
+
+
+    }
+
+}
