9ac5131eb546d0ffe4b1ca8c0a2003a32add085e
ffolabear <kth6699@naver.com>
2022-03-21 04:48:36 +0900

    백준 Brute force - 16198 에너지 모으기

--- src/baekjoon/bruteforce/Boj_16198.java
+++ src/baekjoon/bruteforce/Boj_16198.java
@@ -3,23 +3,55 @@ package baekjoon.bruteforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Boj_16198 {
 
+    static int res = Integer.MIN_VALUE;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] nums = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int index = 0;
+        ArrayList<Integer> nums = new ArrayList<>();
+
+
         while (st.hasMoreTokens()) {
-            nums[index++] = Integer.parseInt(st.nextToken());
+            nums.add(Integer.parseInt(st.nextToken()));
         }
 
+
+        dfs(nums, 0);
+
+        System.out.println(res);
+
+    }
+
+    private static void dfs(ArrayList<Integer> nums, int sum) {
+
+        if (nums.size() <= 2) {
+            if (res < sum) {
+                res = sum;
+            }
+            return;
+        }
+
+        for (int i = 1; i < nums.size() - 1; i++) {
+
+            int current = nums.get(i);
+            int gather = nums.get(i - 1) * nums.get(i + 1);
+
+            nums.remove(i);
+            sum += gather;
+
+            dfs(nums, sum);
+            nums.add(i, current);
+        }
 
 
     }
