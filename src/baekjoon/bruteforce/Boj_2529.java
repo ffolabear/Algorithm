0c023b838f8b01acd85a76d05f08bbc1c8fd530b
ffolabear <kth6699@naver.com>
2022-03-21 23:57:06 +0900

    백준 Brute force - 2529 부등호 푸는중

---
+++ src/baekjoon/bruteforce/Boj_2529.java
@@ -0,0 +1,29 @@
+package baekjoon.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_2529 {
+
+    static int N;
+    static int[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    static int min = Integer.MAX_VALUE;
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+
+
+    }
+
+    static void dfs() {
+
+    }
+
+}
