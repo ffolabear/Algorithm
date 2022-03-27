5f4f71ecbd714d8f9836a2bf6d36740a9a27ee61
ffolabear <kth6699@naver.com>
2022-03-27 16:41:55 +0900

    인프런 강의 문제풀이중 - String 완료

--- src/Lecture/string/ShortestDistance.java
+++ src/Lecture/string/ShortestDistance.java
@@ -7,26 +7,38 @@ public class ShortestDistance {
 
     private String solution(String s, String t) {
 
-        ArrayList<Integer> ans = new ArrayList<>();
+        int[] ans = new int[s.length()];
+        int[] ans1 = new int[s.length()];
         StringBuilder sb = new StringBuilder();
 
-        int lastIdx = 0;
+        String[] str = s.split("");
 
+        int p = 1000;
         for (int i = 0; i < s.length(); i++) {
-
-            String current = String.valueOf(s.charAt(i));
-            if (current.equals(t)) {
-                ans.add(0);
-                lastIdx = i;
+            if (str[i].equals(t)) {
+                p = 0;
+                ans[i] = p++;
             } else {
-
+                ans[i] = p++;
             }
         }
 
-        for (int i : ans) {
-            sb.append(i).append(" ");
+        p = 1000;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (str[i].equals(t)) {
+                p = 0;
+                ans1[i] = p++;
+            } else {
+                ans1[i] = p++;
+            }
+
         }
 
+
+        for (int i = 0; i < ans.length; i++) {
+            sb.append(Math.min(ans[i], ans1[i])).append(" ");
+        }
 
         return sb.toString();
     }
