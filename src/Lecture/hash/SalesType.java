cfec30113385a00745f39a0c5feb2f402a8a18f5
ffolabear <kth6699@naver.com>
2022-04-03 16:39:01 +0900

    인프런 강의 문제풀이중 - Hash 푸는중

---
+++ src/Lecture/hash/SalesType.java
@@ -0,0 +1,20 @@
+package Lecture.hash;
+
+import java.util.Scanner;
+
+public class SalesType {
+
+    private void solution(int[] sales, int K) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int K = in.nextInt();
+        int[] sales = new int[N];
+
+
+    }
+
+}
