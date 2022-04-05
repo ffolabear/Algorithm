bc62487dca918bae60e31cd8941044920955a2ed
ffolabear <kth6699@naver.com>
2022-04-05 14:15:14 +0900

    인프런 강의 문제풀이중 - Hash 푸는중

--- src/Lecture/hash/SalesType.java
+++ src/Lecture/hash/SalesType.java
@@ -23,33 +23,23 @@ public class SalesType {
             }
         }
         ans.add(type.size());
-        System.out.println();
-        System.out.println(type);
-        System.out.println();
 
-        for (int i = K ; i < sales.length; i++) {
-            System.out.println("[" + i + "] " +  "시작 지점 : " + type);
-            System.out.println(" 현재 항 : " + sales[i] + " 포함 여부 : " + type.containsKey(sales[i]));
+        for (int i = K; i < sales.length; i++) {
             if (type.containsKey(sales[i])) {
                 type.put(sales[i], type.get(sales[i]) + 1);
             } else {
                 type.put(sales[i], 1);
             }
 
-            System.out.println("추가 후 : " + type);
-            System.out.println("제거 대상 : " + sales[i - K] + " 포함 여부 : " + type.containsKey(sales[i - K]) + " 해당 항: " + type.get(sales[i - K]));
+            if (type.get(sales[i - K]) > 1) {
+                type.put(sales[i - K], type.get(sales[i - K]) - 1);
+            } else {
+                type.remove(sales[i - K]);
 
-                if (type.get(sales[i - K]) > 1) {
-                    type.put(sales[i - K], type.get(sales[i - K]) - 1);
-                } else {
-                    type.remove(sales[i - K]);
-
-                }
+            }
 
 
-            System.out.println("첫 항 제거 : " + type);
             ans.add(type.size());
-            System.out.println();
         }
 
 
