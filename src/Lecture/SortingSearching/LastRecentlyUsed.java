2fad9cff68741a64917c6aaae38f444881ce32ba
ffolabear <kth6699@naver.com>
2022-04-16 17:22:24 +0900

    인프런 강의 문제풀이중 - Sorting, Searching 푸는중

---
+++ src/Lecture/SortingSearching/LastRecentlyUsed.java
@@ -0,0 +1,21 @@
+package Lecture.SortingSearching;
+
+import java.util.Scanner;
+
+public class LastRecentlyUsed {
+
+    private void solution(int S, int[] tasks) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int S = in.nextInt();
+        int N = in.nextInt();
+        int[] tasks = new int[N];
+
+        LastRecentlyUsed sol = new LastRecentlyUsed();
+        sol.solution(S, tasks);
+
+    }
+}
