20d50d541bc236a4effed1fa831a29da7c33a09f
ffolabear <kth6699@naver.com>
2022-04-17 06:26:12 +0900

    인프런 강의 문제풀이중 - Sorting, Searching 푸는중

--- src/Lecture/SortingSearching/LastRecentlyUsed.java
+++ src/Lecture/SortingSearching/LastRecentlyUsed.java
@@ -4,76 +4,41 @@ import java.util.*;
 
 public class LastRecentlyUsed {
 
-    static int[] arr;
 
     private void solution(int S, int[] tasks) {
 
-        arr = new int[S];
+        int[] arr = new int[S];
+        for (int i : tasks) {
 
-        for (int i = 0; i < tasks.length; i++) {
-            System.out.println("현재작업 : " + tasks[i]);
+            int idx = -1;
 
-            if (find(tasks[i])) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] == i) {
+                    idx = j;
+                }
             }
 
-//            for (int j = 0; j < arr.length; j++) {
-//                if (arr[j] == tasks[i]) {
-//                    cacheHit(tasks[i], j);
-//                    System.out.println();
-//                    break;
-//                }
-//            }
-//
-//            cacheMiss(tasks[i]);
-//            System.out.println();
-        }
+            if (idx == -1) {
+                for (int k = arr.length - 1; k > 0; k--) {
+                    arr[k] = arr[k - 1];
+                }
+                arr[0] = i;
+            } else {
 
-    }
+                for (int k = idx; k > 0; k--) {
+                    arr[k] = arr[k - 1];
+                }
+                arr[0] = i;
 
-    private boolean find(int task) {
-
-        boolean isValid = false;
-
-        for (int j = 0; j < arr.length; j++) {
-            if (arr[j] == task) {
-                isValid = true;
-                break;
             }
+
+
         }
 
-        return isValid;
-    }
-
-    private void cacheMiss(int task) {
-
-        for (int i = arr.length - 1; i > 0; i--) {
-            arr[i] = arr[i - 1];
+        for (int l : arr) {
+            System.out.print(l + " ");
         }
 
-        arr[0] = task;
-        System.out.print("MISS : ");
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-    }
-
-    private void cacheHit(int task, int idx) {
-
-        System.out.println("[" + idx + "]");
-
-        for (int i = idx; i > 0; i--) {
-            arr[i] = arr[i - 1];
-        }
-
-        arr[0] = task;
-
-        System.out.print("MISS : ");
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
 
     }
 
