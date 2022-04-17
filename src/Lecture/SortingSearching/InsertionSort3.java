c93c0c3c1fdbd51ee32f7ac2a25ba4e3ffc4df3d
ffolabear <kth6699@naver.com>
2022-04-17 16:24:15 +0900

    인프런 강의 문제풀이중 - Sorting, Searching 푸는중

---
+++ src/Lecture/SortingSearching/InsertionSort3.java
@@ -0,0 +1,41 @@
+package Lecture.SortingSearching;
+
+import java.util.Scanner;
+
+public class InsertionSort3 {
+
+    private void solution(int[] nums) {
+
+        for (int i = 1; i < nums.length; i++) {
+
+            int j;
+            int temp = nums[i];
+
+            //현재 대상보다 작은 원소중에서
+            //이전원소가 현재 원소보다 클경우를 찾기 위해서 반복문
+            for (j = i; j > 0 && nums[j - 1] > temp; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[j] = temp;
+
+        }
+
+        for (int k : nums) {
+            System.out.print(k + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = in.nextInt();
+        }
+
+        InsertionSort3 sol = new InsertionSort3();
+        sol.solution(nums);
+
+    }
+
+}
