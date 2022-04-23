90f398fd74f5b3f689cc35a7cb21222e1ded1443
ffolabear <kth6699@naver.com>
2022-04-23 23:11:55 +0900

    인프런 강의 문제풀이중 - Recursive, Tree, Graph(DFS, BFS 기초) 푸는중

---
+++ src/Lecture/Recursive/RecursiveFunction1.java
@@ -0,0 +1,29 @@
+package Lecture.Recursive;
+
+import java.util.Scanner;
+
+public class RecursiveFunction1 {
+
+
+    private void solution(int N) {
+
+        if (N == 0) {
+            return;
+        } else {
+            System.out.println(N);
+            solution(N - 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        RecursiveFunction1 sol = new RecursiveFunction1();
+        sol.solution(N);
+
+
+    }
+
+}
