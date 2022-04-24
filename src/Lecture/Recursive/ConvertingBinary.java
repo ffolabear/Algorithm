30c1c92b029992c2d0d33b3f0a120cfc1acd78b5
ffolabear <kth6699@naver.com>
2022-04-24 23:15:02 +0900

    인프런 강의 문제풀이중 - Recursive, Tree, Graph(DFS, BFS 기초) 푸는중

---
+++ src/Lecture/Recursive/ConvertingBinary.java
@@ -0,0 +1,27 @@
+package Lecture.Recursive;
+
+import java.util.Scanner;
+
+public class ConvertingBinary {
+
+    private void solution(int N) {
+
+        if (N > 0) {
+            solution(N / 2);
+            System.out.print(N % 2);
+        } else if (N == 0){
+            return;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        ConvertingBinary sol = new ConvertingBinary();
+        sol.solution(N);
+
+
+    }
+}
