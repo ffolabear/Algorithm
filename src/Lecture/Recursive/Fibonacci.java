e54079369dda7a9f7580f4bc139d364606d206fa
ffolabear <kth6699@naver.com>
2022-04-25 21:57:01 +0900

    인프런 강의 문제풀이중 - Recursive, Tree, Graph(DFS, BFS 기초) 푸는중

--- src/Lecture/Recursive/Fibonacci.java
+++ src/Lecture/Recursive/Fibonacci.java
@@ -6,14 +6,32 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
-    private int solution(int N) {
+    static int[] fibo;
 
-        if (N == 1) {
-            return 1;
-        } else if (N == 2) {
-            return 1;
+    //재귀의 안좋은 예 - 시간이 오래걸림
+    //배열을 사용하는게 더 좋음
+    private int solution(int idx) {
+
+//        if (N == 1) {
+//            return 1;
+//        } else if (N == 2) {
+//            return 1;
+//        } else {
+//            return solution(N - 1) + solution(N - 2);
+//        }
+
+        //메모이 제이션의 활용
+        if (fibo[idx] > 0) {
+            return fibo[idx];
+        }
+
+        //연산결과를 기록해 놓는것 -> 메모이제이션
+        if (idx == 1) {
+            return fibo[idx] = 1;
+        } else if (idx == 2) {
+            return fibo[idx] = 1;
         } else {
-            return solution(N - 1) + solution(N - 2);
+            return fibo[idx] = solution(idx - 1) + solution(idx - 2);
         }
 
 
@@ -25,13 +43,14 @@ public class Fibonacci {
         int N = in.nextInt();
 
         Fibonacci sol = new Fibonacci();
+        fibo = new int[N + 1];
+
+        sol.solution(N);
 
 
-        for (int i=1; i <= N; i++){
-
-            System.out.print(sol.solution(i) + " ");
+        for (int i : fibo) {
+            System.out.print(i + " ");
         }
-
 
     }
 
