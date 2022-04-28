eba9f7e814a1147abeff3a3c6f16f246838b8cbf
ffolabear <ffolabear@gmail.com>
2022-04-28 22:54:24 +0900

    인프런 강의 문제풀이중 - Recursive, Tree, Graph(DFS, BFS 기초) 푸는중

---
+++ src/Lecture/bfs/FindingCalf.java
@@ -0,0 +1,26 @@
+package Lecture.bfs;
+
+import java.util.Scanner;
+
+public class FindingCalf {
+
+    static int S;
+    static int E;
+    static int max;
+
+    private void bfs() {
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        S = in.nextInt();
+        E = in.nextInt();
+        max = E - S;
+
+    }
+
+}
