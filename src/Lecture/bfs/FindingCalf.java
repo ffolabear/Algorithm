83234fb1a4f581c0bd6115f2696a4b0e483546f0
ffolabear <ffolabear@gmail.com>
2022-04-29 04:34:25 +0900

    인프런 강의 문제풀이중 - Recursive, Tree, Graph(DFS, BFS 기초) 푸는중

--- src/Lecture/bfs/FindingCalf.java
+++ src/Lecture/bfs/FindingCalf.java
@@ -1,25 +1,47 @@
 package Lecture.bfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class FindingCalf {
 
-    static int S;
     static int E;
     static int max;
+    static int temp = Integer.MIN_VALUE;
 
-    private void bfs() {
+    private void bfs(int S) {
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(S);
+
+        int L = 0;
+        while (!queue.isEmpty()) {
+
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                int current = queue.poll();
+
+                queue.offer(current - 1);
+                queue.offer(current + 1);
+                queue.offer(current + 5);
+                L++;
+
+            }
 
 
+        }
 
     }
 
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        S = in.nextInt();
+        int S = in.nextInt();
         E = in.nextInt();
         max = E - S;
+        FindingCalf sol = new FindingCalf();
+        sol.bfs(S);
 
     }
 
