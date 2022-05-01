7aac7a1dd4f25eaede678b66a1e56fe49d1eb767
ffolabear <ffolabear@gmail.com>
2022-05-01 23:56:50 +0900

    인프런 강의 문제풀이중 - Recursive, Tree, Graph(DFS, BFS 기초) 푸는중

---
+++ src/Lecture/dfs/DFS2.java
@@ -0,0 +1,31 @@
+package Lecture.dfs;
+
+/**
+ * DFS 구현 익숙해질때까지 반복
+ */
+
+public class DFS2 {
+
+    static Tree tree = new Tree();
+
+    private void DFS(Node root) {
+
+        if (root == null) {
+            return;
+        } else {
+
+            DFS(root.lt);
+            DFS(root.rt);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Node root = tree.root;
+        DFS2 sol = new DFS2();
+        sol.DFS(root);
+
+    }
+}
