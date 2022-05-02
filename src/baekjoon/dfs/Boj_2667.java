01cdc94b65802a5a439559b57483f8aae9555a5c
ffolabear <ffolabear@gmail.com>
2022-05-02 22:52:36 +0900

    DFS 복습 - 백준 2667 단지 번호 붙이기 푸는중

---
+++ src/baekjoon/dfs/Boj_2667.java
@@ -0,0 +1,34 @@
+package baekjoon.dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_2667 {
+
+    static int N;
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static boolean[][] visited;
+
+    static void dfs(int x, int y) {
+
+    }
+
+    static void pro() {
+
+
+
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+
+    }
+
+}
