7db7be654bff78011178a9fc7c8f10c26059cb14
ffolabear <ffolabear@gmail.com>
2022-05-06 03:29:37 +0900

    DFS 복습 - 백준 2667 단지 번호 붙이기 푸는중

---
+++ src/Lecture/bfs/MovingNodeBfs.java
@@ -0,0 +1,52 @@
+package Lecture.bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MovingNodeBfs {
+
+
+    static Tree tree = new Tree();
+
+    private int BFS(Node root) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                Node current = queue.poll();
+
+                if (current.lt == null && current.rt == null) {
+                    return level;
+                }
+                if (current.lt != null) {
+                    queue.offer(current.lt);
+                }
+
+                if (current.rt != null) {
+                    queue.offer(current.rt);
+                }
+            }
+
+            //큐가 아직 비어있지 않으므로 탐색이 더 진행됨
+            level++;
+        }
+
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+
+        Node root = tree.root;
+        MovingNodeBfs sol = new MovingNodeBfs();
+
+        System.out.println(sol.BFS(root));
+    }
+
+
+}
--- src/Lecture/dfs/MovingNodeDfs.java
+++ src/Lecture/dfs/MovingNodeDfs.java
@@ -1,18 +1,15 @@
 package Lecture.dfs;
 
-import java.util.Map;
-
-public class MovingNode {
+public class MovingNodeDfs {
 
 
     static Tree tree = new Tree();
 
     private int DFS(Node root, int level) {
         if (root.rt == null && root.lt == null) {
-            System.out.println(level + " : " + root.data);
             return level;
         } else {
-            return Math.min(DFS(root.lt, level++), DFS(root.rt, level++));
+            return Math.min(DFS(root.lt, level + 1), DFS(root.rt, level + 1));
 
 
         }
@@ -23,8 +20,9 @@ public class MovingNode {
     public static void main(String[] args) {
 
         Node root = tree.root;
-        MovingNode sol = new MovingNode();
-        sol.DFS(root, 0);
+        MovingNodeDfs sol = new MovingNodeDfs();
+
+        System.out.println(sol.DFS(root, 0));
     }
 
 
