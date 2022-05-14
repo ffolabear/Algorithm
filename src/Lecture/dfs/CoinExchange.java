b66b9850f56633c17ec5a0ccdea8ba3db72c2e0d
ffolabear <ffolabear@gmail.com>
2022-05-14 23:53:58 +0900

    DFS, BFS 활용 - 동전교환(DFS) - 시간초과 해결중 (배열 역정렬)

--- src/Lecture/dfs/CoinExchange.java
+++ src/Lecture/dfs/CoinExchange.java
@@ -1,17 +1,24 @@
 package Lecture.dfs;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class CoinExchange {
 
     static int N, M;
-    static int[] coins;
+    static Integer[] coins;
 
     static int ans = Integer.MAX_VALUE;
 
     private void dfs(int amount, int sum) {
 
         if (sum > M) {
+            return;
+        }
+
+        if (amount >= ans) {
             return;
         }
 
@@ -31,12 +38,14 @@ public class CoinExchange {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         N = in.nextInt();
-        coins = new int[N];
+        coins = new Integer[N];
 
         for (int i = 0; i < coins.length; i++) {
             coins[i] = in.nextInt();
         }
         M = in.nextInt();
+
+        Arrays.sort(coins, Collections.reverseOrder());
 
         CoinExchange sol = new CoinExchange();
         sol.dfs(0, 0);
