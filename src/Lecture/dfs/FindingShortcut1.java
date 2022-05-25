4e338d1a4bf1b6461e37192b59e759f6adf264d8
ffolabear <ffolabear@gmail.com>
2022-05-25 22:50:12 +0900

    DFS, BFS 활용 - 미로의 최단거리 통로(BFS) 푸는중

--- src/Lecture/dfs/FindingShortcut1.java
+++ src/Lecture/dfs/FindingShortcut1.java
@@ -1,19 +1,51 @@
 package Lecture.dfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+
+class node{
+    int x, y;
+
+    public node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class FindingShortcut1 {
 
-    static int[][] maze = new int[8][8];
+
+
+    static int[][] maze = new int[7][7];
+    static int[][] dis = new int[7][7];
+
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
     static int ans = Integer.MAX_VALUE;
 
-    private void bfs(int x, int y) {
+    private void bfs(int x, int y, int sum) {
 
         if (x == 7 && y == 7) {
+            ans = Math.max(ans, sum);
+        } else {
+            for (int i = 0; i < 4; i++) {
+                Queue<int[]> queue = new LinkedList<>();
 
+                int nx = x + dx[i];
+                int ny = x + dy[i];
+                int[] temp = new int[2];
+
+                if (nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && maze[nx][ny] == 1) {
+                    temp[0] = nx;
+                    temp[1] = ny;
+                    queue.offer(temp);
+
+
+                }
+
+            }
         }
 
     }
@@ -30,7 +62,7 @@ public class FindingShortcut1 {
 
         FindingShortcut1 sol = new FindingShortcut1();
         maze[1][1] = 1;
-        sol.bfs(1, 1);
+        sol.bfs(1, 1, 0);
         System.out.println(ans);
 
     }
