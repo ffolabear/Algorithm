c1e89292f7fa58ce652e768ba96f2f2399bbf310
ffolabear <ffolabear@gmail.com>
2022-05-29 23:57:39 +0900

    DFS, BFS 활용 - 미로의 최단거리 통로(BFS) 복습, 토마토(BFS 활용) 푸는중

--- src/Lecture/bfs/Tomato.java
+++ src/Lecture/bfs/Tomato.java
@@ -1,16 +1,40 @@
 package Lecture.bfs;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Tomato {
 
     static int[][] box;
-    static boolean[][] isVisited;
+    static int[][] chk;
+
+    static List<Node> tomatoes = new ArrayList<>();
 
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    Queue<Node> queue = new LinkedList<>();
 
     private void bfs() {
+
+        for (Node node : tomatoes) {
+
+            queue.offer(node);
+            chk[node.x][node.y] = 0;
+
+            while (!queue.isEmpty()) {
+
+                Node current = queue.poll();
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = current.x + dx[i];
+                    int ny = current.y + dy[i];
+
+
+                }
+            }
+
+
+
+        }
 
     }
 
@@ -20,11 +44,14 @@ public class Tomato {
         int N = in.nextInt();
 
         box = new int[N + 1][M + 1];
-        isVisited = new boolean[N + 1][M + 1];
+        chk = new int[N + 1][M + 1];
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
                 box[i][j] = in.nextInt();
+                if (box[i][j] == 1) {
+                    tomatoes.add(new Node(i, j));
+                }
             }
         }
 
@@ -36,6 +63,10 @@ public class Tomato {
 //        }
 
         Tomato sol = new Tomato();
+        System.out.println(tomatoes);
+        for (Node n : tomatoes) {
+            System.out.println(n.x + " " + n.y);
+        }
         sol.bfs();
 
     }
