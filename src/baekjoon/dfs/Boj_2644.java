4ac86c72f3c2d566daf7eac3d0c28a19831ba6da
ffolabear <ffolabear@gmail.com>
2022-06-03 03:59:07 +0900

    DFS, BFS 활용 - 백준 촌수계산 2644

--- src/baekjoon/dfs/Boj_2644.java
+++ src/baekjoon/dfs/Boj_2644.java
@@ -1,36 +1,67 @@
 package baekjoon.dfs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Boj_2644 {
 
     static int n, a, b, m;
     static boolean[] visited;
     static ArrayList<Integer>[] node;
+    static int ans = -1;
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    private void dfs(int start, int end, int count) {
+        if (start == end) {
+            ans = count;
+            return;
+        } else {
+            visited[start] = true;
+            for (int i = 0; i < node[start].size(); i++) {
+                int current = node[start].get(i);
+                if (!visited[current]) {
+                    dfs(current, end, count + 1);
+                }
 
-        n = in.nextInt();
-        a = in.nextInt();
-        b = in.nextInt();
-        m = in.nextInt();
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        m = Integer.parseInt(br.readLine());
 
         node = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
 
         for (int i = 1; i <= n; i++) {
             node[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i <= n; i++) {
-            int from = in.nextInt();
-            int to = in.nextInt();
-            node[from].add(to);
-            node[to].add(from);
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int parent = Integer.parseInt(st.nextToken());
+            int child = Integer.parseInt(st.nextToken());
+            node[parent].add(child);
+            node[child].add(parent);
         }
 
-        System.out.println(n + " " + a + " " + b);
-        System.out.println(node);
+
+
+        Boj_2644 sol = new Boj_2644();
+        sol.dfs(a, b, 0);
+        System.out.println(ans);
     }
 }
