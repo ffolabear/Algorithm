175b1901bf617e101b9184007b4c39a6c8e2afec
ffolabear <ffolabear@gmail.com>
2022-06-22 02:58:22 +0900

    Programmers lv2 - 문자열 압축

--- src/Programmers/level02_2/StringCompression.java
+++ src/Programmers/level02_2/StringCompression.java
@@ -4,33 +4,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * aabbaccc
- * a2b2
- *
- */
-
 public class StringCompression {
 
     public int solution(String s) {
 
+        int answer = s.length();
+
         //가장 긴 단위는 절반보다 클수 없음
         int maxLen = s.length() / 2;
 
-        if (s.length() == 1) {
-            return 1;
-        } else {
+        for (int i = 1; i <= maxLen; i++) {
 
-            for (int i = 1; i <= maxLen; i++) {
+            String pattern = s.substring(0, i);
+            String remain = "";
+            int count = 1;
+            StringBuilder sb = new StringBuilder();
 
+
+            for (int j = i; j <= s.length(); j += i) {
+
+                if (i + j >= s.length()) {
+                    remain = s.substring(j);
+                } else {
+                    remain = s.substring(j, j + i);
+                }
+
+                if (pattern.equals(remain)) {
+                    //만약 현재 비교하는 패턴과 다음 비교 대상이 같은경우
+                    count++;
+
+                } else if (count == 1) {
+                    //만약 현재 비교하는 패턴과 다음 비교 대상이 다르지만 카운트가 1인 경우
+                    sb.append(pattern);
+                    pattern = remain;
+                } else {
+                    //만약 현재 비교하는 패턴과 다음 비교 대상이 다르지만 카운트가 1이 아닌경우
+                    sb.append(count).append(pattern);
+                    pattern = remain;
+                    count = 1;
+                }
             }
 
+            //압축후 나머지 붙여줌
+            if (i != s.length()) {
+                sb.append(pattern);
 
+            }
+            answer = Math.min(answer, sb.length());
 
         }
 
-
-        int answer = 0;
         return answer;
     }
 
