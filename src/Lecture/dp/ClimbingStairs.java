6d52dbc1a928e208c67aeafb0853dbbe3e4b0c64
ffolabear <ffolabear@gmail.com>
2022-06-25 23:59:28 +0900

    DP - 계단 오르기 푸는중

---
+++ src/Lecture/dp/ClimbingStairs.java
@@ -0,0 +1,13 @@
+package Lecture.dp;
+
+import java.util.Scanner;
+
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+    }
+
+}
