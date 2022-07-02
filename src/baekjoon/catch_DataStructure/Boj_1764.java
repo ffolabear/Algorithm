e5239a31dfd36f61a81ad1254be589619f62c1fd
ffolabear <ffolabear@gmail.com>
2022-07-02 13:19:43 +0900

    백준 1764 - 듣보잡

---
+++ src/baekjoon/catch_DataStructure/Boj_1764.java
@@ -0,0 +1,44 @@
+package baekjoon.catch_DataStructure;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Boj_1764 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+
+        Map<String, Integer> neverHeard = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            String name = br.readLine();
+            neverHeard.put(name, 0);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if (neverHeard.containsKey(name)) {
+                bw.append(name).append('\n');
+                ans += 1;
+            }
+        }
+
+        System.out.println(ans);
+        bw.flush();
+        bw.close();
+
+    }
+
+}
