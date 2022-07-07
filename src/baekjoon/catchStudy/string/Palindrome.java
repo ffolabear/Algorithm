e497dea9df260c287a9773d82f30864e18f2c188
ffolabear <ffolabear@gmail.com>
2022-07-07 18:39:48 +0900

    백준 17609 - 회문 푸는중

--- src/baekjoon/catchStudy/string/Palindrome.java
+++ src/baekjoon/catchStudy/string/Palindrome.java
@@ -7,23 +7,19 @@ public class Palindrome {
     private static int isValid(String input) {
         System.out.println(input);
         StringBuilder sb = new StringBuilder(input);
+
         String fromHead = sb.toString();
         String fromBackward = sb.reverse().toString();
+
+        String halfHead = fromHead.substring(0, input.length()/2);
+        String halfBackward = fromBackward.substring(0, input.length()/2);
 
         if (fromHead.equals(fromBackward)) {
             return 0;
         } else {
-            int cnt = 0;
-            for (int i = 0; i < fromHead.length(); i++) {
-                if (fromHead.charAt(i) != fromBackward.charAt(i)) {
-                    cnt++;
-                }
-            }
-            if (cnt == 2) {
-                return 1;
-            } else {
-                return 2;
-            }
+
+            return 0;
+
         }
 
     }
