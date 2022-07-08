2c9bf788412e49ffb5ed47e4c9a8b5ae552f8867
ffolabear <ffolabear@gmail.com>
2022-07-08 22:52:10 +0900

    백준 17609 - 회문

--- src/baekjoon/catchStudy/string/Palindrome.java
+++ src/baekjoon/catchStudy/string/Palindrome.java
@@ -1,45 +1,60 @@
 package baekjoon.catchStudy.string;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Palindrome {
 
-    private static int isValid(String input) {
-        System.out.println(input);
-        StringBuilder sb = new StringBuilder(input);
+    static int t;
+    static String s;
+    static char[] arr;
+    public static void main(String[] args) {
 
-        String fromHead = sb.toString();
-        String fromBackward = sb.reverse().toString();
-
-        String halfHead = fromHead.substring(0, input.length()/2);
-        String halfBackward = fromBackward.substring(0, input.length()/2);
-
-        if (fromHead.equals(fromBackward)) {
-            return 0;
-        } else {
-
-            return 0;
+        Scanner sc=new Scanner(System.in);
+        t=sc.nextInt();
+        for (int tc = 1; tc <=t; tc++) {
+            s=sc.next();
+            arr=s.toCharArray();
+            int left=0;
+            int right=s.length()-1;
+            if(check(left,right)) {
+                System.out.println(0);
+                continue;
+            }
+            if(checkS(left,right)) {
+                System.out.println(1);
+            }else {
+                System.out.println(2);
+            }
 
         }
 
     }
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-
-
-        for (int i = 0; i < N; i++) {
-            String input = br.readLine();
-            bw.append(String.valueOf(isValid(input))).append('\n');
+    private static boolean check(int left,int right) {
+        while(left<=right) {
+            if(arr[left]!=arr[right]) {//다름
+                return false;
+            }
+            left+=1;
+            right-=1;
         }
+        return true;
+    }
 
-        bw.flush();
-        bw.close();
-
+    private static boolean checkS(int left,int right) {
+        while(left<=right) {
+            if(arr[left]!=arr[right]) {//다름
+                boolean a=check(left+1,right);
+                boolean b=check(left,right-1);
+                if(a==false && b==false) {
+                    return false;
+                }else return true;
+            }
+            left+=1;
+            right-=1;
+        }
+        return true;
     }
 
 }
