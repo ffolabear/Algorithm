785a510ff66508b09bbfbdf57ba373e631197738
ffolabear <ffolabear@gmail.com>
2022-07-21 23:58:31 +0900

    백준 5675 - 시침과 분침 푸는중

--- src/JuniorDeveloperEducation/day3/Boj_5675.java
+++ src/JuniorDeveloperEducation/day3/Boj_5675.java
@@ -11,12 +11,40 @@ public class Boj_5675 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String input = "";
-        while (input != null) {
+        while (true) {
             input = br.readLine();
+            int time = Integer.parseInt(input);
+            System.out.println(isValid(time));
+        }
+    }
+
+    private static String isValid(int time) {
+
+        int degree = 0;
+        String ans = "N";
+
+        int hour = 0;
+        int min = 0;
+
+        while (hour < 13) {
+
+            if (degree == time) {
+                ans = "Y";
+                break;
+            }
+
+            if (min > 360) {
+                min = min - 360;
+            }
+
+            degree = Math.abs(min - hour);
+
+            hour += 1;
+            min += 12;
         }
 
 
+        return ans;
     }
-
 
 }
