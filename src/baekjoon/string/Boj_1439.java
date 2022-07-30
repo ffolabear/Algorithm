ff525e35041c8cdcaa2f9c023b30c8527377e936
ffolabear <ffolabear@gmail.com>
2022-07-30 23:59:21 +0900

    백준 1439 - 뒤집기 푸는중

---
+++ src/baekjoon/string/Boj_1439.java
@@ -0,0 +1,22 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_1439 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int zero = 0;
+        int one = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+        }
+
+
+    }
+
+}
