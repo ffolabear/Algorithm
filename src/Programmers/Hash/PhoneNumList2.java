a438cd037d1aba9fc751c903dc83d0a0e619ef80
ffolabear <ffolabear@gmail.com>
2022-08-01 23:59:27 +0900

    프로그래머스 lv2 - 전화번호 목록 푸는중

---
+++ src/Programmers/Hash/PhoneNumList2.java
@@ -0,0 +1,33 @@
+package Programmers.Hash;
+
+
+import java.util.HashMap;
+
+public class PhoneNumList2 {
+
+    public boolean solution(String[] phone_book) {
+
+        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < phone_book.length; i++) {
+            map.put(phone_book[i], 0);
+        }
+
+        for (int i = 0; i < phone_book.length; i++) {
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        String[] nums = {"119", "97674223", "1195524421"};
+        PhoneNumList2 phoneNumList = new PhoneNumList2();
+
+        System.out.println(phoneNumList.solution(nums));
+
+
+    }
+}
