de36b59d505bbc3d1bbdc1b94a5f30f37474c9e5
ffolabear <ffolabear@gmail.com>
2022-08-02 02:24:35 +0900

    프로그래머스 lv2 - 전화번호 목록

--- src/Programmers/Hash/PhoneNumList2.java
+++ src/Programmers/Hash/PhoneNumList2.java
@@ -15,7 +15,12 @@ public class PhoneNumList2 {
         }
 
         for (int i = 0; i < phone_book.length; i++) {
-
+            for (int j = 1; j < phone_book[i].length(); j++) {
+                String temp = phone_book[i].substring(0, j);
+                if (map.containsKey(temp)) {
+                    answer = false;
+                }
+            }
         }
 
         return answer;
