7255f97266cc0dc0df38004faa9cf205c416c51c
ffolabear <ffolabear@gmail.com>
2022-08-02 17:30:44 +0900

    주니어 개발자 실무역량 교육 - 9일차

---
+++ src/Programmers/greedy/TrainingClothes.java
@@ -0,0 +1,36 @@
+package Programmers.greedy;
+
+public class TrainingClothes {
+
+    public int solution(int n, int[] lost, int[] reserve) {
+
+        int answer = n - lost.length;
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+
+
+
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+//        int n = 5;
+//        int[] lost = {2, 4};
+//        int[] reserve = {1, 3, 5};
+
+        int n = 5;
+        int[] lost = {2, 4};
+        int[] reserve = {3};
+
+        TrainingClothes sol = new TrainingClothes();
+        sol.solution(n, lost, reserve);
+
+
+    }
+
+}
