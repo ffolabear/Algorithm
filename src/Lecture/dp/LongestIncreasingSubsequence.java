7d3e73e194562a9a3220815a1d9d2368d0fa77e7
ffolabear <ffolabear@gmail.com>
2022-08-20 23:55:14 +0900

    인프런 dp 강의문제 - LIS 푸는중

---
+++ src/Lecture/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,30 @@
+package Lecture.dp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LongestIncreasingSubsequence {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        //입력부
+        int[] nums = new int[N];
+
+        //[i]번째 숫자를 마지막 항으로 하는 최대 길이의 수열
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        arr[0] = 1;
+
+
+
+
+    }
+
+}
