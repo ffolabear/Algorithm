b080ae3615206f681918524e6737f3f25267a870
ffolabear <ffolabear@gmail.com>
2022-08-29 03:33:14 +0900

    인프런 greedy 강의문제 - 회의실배정

--- src/Lecture/greedy/MeetingRoom2.java
+++ src/Lecture/greedy/MeetingRoom2.java
@@ -34,7 +34,30 @@ public class MeetingRoom2 {
 
 
 
+        Collections.sort(meeting, new Comparator<Meeting>() {
 
+            @Override
+            public int compare(Meeting o1, Meeting o2) {
+                if (o1.end == o2.end) {
+                    return o1.start - o2.start;
+                } else {
+                    return o1.end - o2.end;
+                }
+            }
+        });
+
+        int cnt = 0;
+        int last = 0;
+
+        for (Meeting m : meeting) {
+            if (m.start >= last) {
+                cnt++;
+                last = m.end;
+            }
+        }
+
+
+        System.out.println(cnt);
 
     }
 }
