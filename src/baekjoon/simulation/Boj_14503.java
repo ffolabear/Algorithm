2ba0cba4d339f38d2ff4e918eb4558da1ac60fdb
ffolabear <ffolabear@gmail.com>
2022-08-30 23:04:14 +0900

    백준 14503 - 로봇 청소기 푸는중

---
+++ src/baekjoon/simulation/Boj_14503.java
@@ -0,0 +1,31 @@
+package baekjoon.simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_14503 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] place = new int[N][M];
+
+        st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int direction = Integer.parseInt(st.nextToken());
+
+
+
+    }
+
+}
