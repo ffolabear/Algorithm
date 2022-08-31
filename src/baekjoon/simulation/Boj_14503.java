cff35b2f3786071d34c6520b45076c0a7d5d03ad
ffolabear <ffolabear@gmail.com>
2022-08-31 17:59:03 +0900

    백준 14503 - 로봇 청소기 푸는중

--- src/baekjoon/simulation/Boj_14503.java
+++ src/baekjoon/simulation/Boj_14503.java
@@ -7,6 +7,12 @@ import java.util.StringTokenizer;
 
 public class Boj_14503 {
 
+    static int N, M, r, c, d;
+    static String[][] place;
+    static int cnt = 1;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,18 +20,60 @@ public class Boj_14503 {
 
         st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] place = new int[N][M];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        place = new String[N][M];
 
         st = new StringTokenizer(br.readLine());
 
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        int direction = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            place[i] = br.readLine().split(" ");
+        }
 
 
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < M; j++) {
+//                System.out.print(place[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
+        dfs(r, c, d);
+        System.out.println(c);
     }
 
+    public static void dfs(int r, int c, int d) {
+        place[r][c] = "-1";
+
+        for (int i = 0; i < 4; i++) {
+            d -= 1;
+            if (d == -1) {
+                d = 3;
+            }
+
+            int nx = r + dx[i];
+            int ny = c + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < M){
+                if (place[nx][ny].equals("0")) {
+                    cnt++;
+                    dfs(nx, ny, d);
+                    return;
+                }
+            }
+        }
+
+        int dir = (d + 2) % 4;
+        int mx = r + dx[dir];
+        int my = c + dy[dir];
+
+        if (mx >= 0 && my >= 0 && mx < N && my <  M && !place[mx][my].equals("1")) {
+            dfs(mx, my, dir);
+        }
+
+    }
 }
