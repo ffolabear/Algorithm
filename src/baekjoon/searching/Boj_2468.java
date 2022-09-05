d8fe3fab3524e9ffa168d7e169e53b41761bdb6d
ffolabear <ffolabear@gmail.com>
2022-09-05 22:46:14 +0900

    백준 2468 - 안전 영역 풀었음

--- src/baekjoon/searching/Boj_2468.java
+++ src/baekjoon/searching/Boj_2468.java
@@ -7,15 +7,19 @@ import java.io.InputStreamReader;
 public class Boj_2468 {
 
     static int N, maxHeight;
+    static int answer = 0;
     static int[][] board;
+    static boolean[][] chk;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
         board = new int[N][N];
-        maxHeight = 0;
 
+        maxHeight = Integer.MIN_VALUE;
 
         for (int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
@@ -25,16 +29,46 @@ public class Boj_2468 {
             }
         }
 
-        for (int i = 0; i < N; i++) {
+
+        for (int i = 0; i <= maxHeight; i++) {
+
+            //탐색 여부 체크 배열
+            chk = new boolean[N][N];
+            int temp = 0;
+
             for (int j = 0; j < N; j++) {
-                System.out.print(board[i][j] + " ");
+                for (int k = 0; k < N; k++) {
+                    if (board[j][k] > i && !chk[j][k]) {
+                        temp += dfs(j, k, i);
+                    }
+                }
             }
-            System.out.println();
+            answer = Math.max(answer, temp);
         }
 
-        System.out.println(maxHeight);
 
-
+        System.out.println(answer);
     }
+
+    private static int dfs(int x, int y, int height) {
+
+        chk[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                if (!chk[nx][ny]) {
+                    if (board[nx][ny] > height) {
+                        dfs(nx, ny, height);
+                    }
+                }
+            }
+        }
+        return 1;
+    }
+
 
 }
