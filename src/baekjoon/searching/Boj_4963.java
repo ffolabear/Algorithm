fbee044363b35da48036a9a08f9788b332bea8b4
ffolabear <ffolabear@gmail.com>
2022-09-07 22:46:41 +0900

    백준 4963 - 섬의 갯수

--- src/baekjoon/searching/Boj_4963.java
+++ src/baekjoon/searching/Boj_4963.java
@@ -1,19 +1,21 @@
 package baekjoon.searching;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Boj_4963 {
 
-    static int w, h;
+    static int w, h, cnt;
     static int[][] map;
     static boolean[][] chk;
+
+    static int[] dx = {-1, 1, 0, 0, 1, -1, 1, -1};
+    static int[] dy = {0, 0, -1, 1, 1, 1, -1, -1};
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         while (true) {
@@ -29,6 +31,7 @@ public class Boj_4963 {
 
             map = new int[h][w];
             chk = new boolean[h][w];
+            cnt = 0;
 
             for (int i = 0; i < h; i++) {
                 String[] input = br.readLine().split(" ");
@@ -37,21 +40,41 @@ public class Boj_4963 {
                 }
             }
 
+
             for (int i = 0; i < h; i++) {
                 for (int j = 0; j < w; j++) {
-                    System.out.print(map[i][j] + " ");
+                    if (!chk[i][j] && map[i][j] == 1) {
+                        cnt += dfs(i, j);
+                    }
                 }
-                System.out.println();
             }
 
-            System.out.println();
+            bw.append(String.valueOf(cnt)).append("\n");
+
 
         }
 
-
-
-
-
+        bw.flush();
+        bw.close();
     }
 
+    private static int dfs(int x, int y) {
+
+        chk[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+
+            if (nx >= 0 && ny >= 0 && nx < h && ny < w) {
+                if (!chk[nx][ny] && map[nx][ny] == 1) {
+                    dfs(nx, ny);
+                }
+            }
+
+        }
+
+        return 1;
+    }
 }
