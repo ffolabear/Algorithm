02754e22b3e2f00c6680988b7593419a7ba9de07
ffolabear <ffolabear@gmail.com>
2022-09-15 15:25:44 +0900

    프로그래머스 lv1 - 성격 유형 검사하기

--- src/Programmers/level01/MBTI_Test.java
+++ src/Programmers/level01/MBTI_Test.java
@@ -6,19 +6,18 @@ import java.util.Map;
 
 /**
  * choices	뜻
- *  [1]	매우 비동의 : 3점
- *  [2]	비동의     : 2점
- *  [3]	약간 비동의 : 1점
- *  [4]	모르겠음   : 0점
- *  [5]	약간 동의  : 1점
- *  [6]	동의      : 2점
- *  [7]	매우 동의  : 3점
- *
- *  [R] vs [T]
- *  [C] vs [F]
- *  [J] vs [M]
- *  [A] vs [N]
- *
+ * [1]	매우 비동의 : 3점
+ * [2]	비동의     : 2점
+ * [3]	약간 비동의 : 1점
+ * [4]	모르겠음   : 0점
+ * [5]	약간 동의  : 1점
+ * [6]	동의      : 2점
+ * [7]	매우 동의  : 3점
+ * <p>
+ * [R] vs [T]
+ * [C] vs [F]
+ * [J] vs [M]
+ * [A] vs [N]
  */
 public class MBTI_Test {
 
@@ -35,14 +34,39 @@ public class MBTI_Test {
                 map.put(currentPersonality, map.getOrDefault(currentPersonality, 0) + current - 4);
             }
         }
-        System.out.println(map);
-        return "";
+
+        String answer = result(map);
+        return answer;
     }
 
     private static String result(Map<Character, Integer> map) {
 
+        StringBuilder sb = new StringBuilder();
+        if (map.getOrDefault('R', 0) >= map.getOrDefault('T', 0)) {
+            sb.append('R');
+        } else {
+            sb.append('T');
+        }
 
-        return "";
+        if (map.getOrDefault('C', 0) >= map.getOrDefault('F', 0)) {
+            sb.append('C');
+        } else {
+            sb.append('F');
+        }
+
+        if (map.getOrDefault('J', 0) >= map.getOrDefault('M', 0)) {
+            sb.append('J');
+        } else {
+            sb.append('M');
+        }
+
+        if (map.getOrDefault('A', 0) >= map.getOrDefault('N', 0)) {
+            sb.append('A');
+        } else {
+            sb.append('N');
+        }
+
+        return sb.toString();
     }
 
     public static void main(String[] args) {
@@ -51,7 +75,6 @@ public class MBTI_Test {
 
         String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
         int[] choices = {5, 3, 2, 7, 5};
-
 
 
 //        String[] survey = {"TR", "RT", "TR"};
