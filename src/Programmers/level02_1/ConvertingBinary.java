31f70139e82c4d24d6c99dfdef1386bb5d4e25bf
ffolabear <ffolabear@gmail.com>
2022-09-17 12:23:10 +0900

    프로그래머스 lv2 - 이진 변환 반복하기

--- src/Programmers/level02_1/ConvertingBinary.java
+++ src/Programmers/level02_1/ConvertingBinary.java
@@ -2,35 +2,53 @@ package Programmers.level02_1;
 
 public class ConvertingBinary {
 
+    static int countZero = 0;
 
     public int[] solution(String s) {
 
         int cnt = 0;
-        int[] answer = {};
-        int countZero = 0;
+        int[] answer = new int[2];
 
-
-        while (s.length() != 1) {
-
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '0') {
-                    countZero++;
-                }
-            }
-            s = s.replaceAll("1", "");
-            System.out.println(s);
+        while (!s.equals("1")) {
+            s = convert(s);
+            cnt++;
         }
 
+        answer[0] = cnt;
+        answer[1] = countZero;
         return answer;
+    }
+
+    public String convert(String s) {
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                countZero++;
+            }
+        }
+
+        s = s.replaceAll("0", "");
+        int length = s.length();
+
+        StringBuilder str = new StringBuilder();
+
+        while (length > 0) {
+            int temp = length % 2;
+            length /= 2;
+            str.insert(0, temp);
+        }
+
+        return str.toString();
     }
 
     public static void main(String[] args) {
 
         ConvertingBinary sol = new ConvertingBinary();
-        String s = "110010101001";
+//        String s = "110010101001";
+//        String s = "01110";
+        String s = "1111111";
 
-        System.out.println(sol.solution(s));
-
+        sol.solution(s);
     }
 
 }
