6e2f7482c0905c942660017ed7f6dc05246965b0
ffolabear <ffolabear@gmail.com>
2022-09-20 14:19:19 +0900

    프로그래머스 lv2 - 영어 끝말잇기

---
+++ src/Programmers/level02_1/WordsGame.java
@@ -0,0 +1,56 @@
+package Programmers.level02_1;
+
+import java.util.HashSet;
+
+public class WordsGame {
+
+    public int[] solution(int n, String[] words) {
+
+        int[] answer = new int[2];
+        HashSet<String> hashSet = new HashSet<>();
+
+        int player = 1;
+        int turn = 1;
+        String last = "";
+
+        for (int i = 0; i < words.length; i++) {
+            if (player > n) {
+                player = 1;
+                turn++;
+            }
+
+            if (hashSet.contains(words[i])) {
+                answer[0] = player;
+                answer[1] = turn;
+                break;
+            } else if (i != 0 && last.charAt(last.length() - 1) != words[i].charAt(0)) {
+                answer[0] = player;
+                answer[1] = turn;
+                break;
+            } else {
+                hashSet.add(words[i]);
+            }
+
+
+            player++;
+            last = words[i];
+        }
+
+        System.out.println(answer[0] + " " + answer[1]);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        WordsGame sol = new WordsGame();
+//        int n = 3;
+//        String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+
+        int n = 2;
+        String[] words = {"hello", "one", "even", "never", "now", "world", "draw"};
+
+        sol.solution(n, words);
+
+    }
+
+}
