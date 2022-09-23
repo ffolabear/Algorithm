673f7dd7142451114ee43260e2e4baff423360a3
ffolabear <ffolabear@gmail.com>
2022-09-23 11:34:47 +0900

    백준 9095 - 1, 2, 3 더하기

--- src/baekjoon/dp/Boj_9095.java
+++ src/baekjoon/dp/Boj_9095.java
@@ -1,8 +1,6 @@
 package baekjoon.dp;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,14 +9,24 @@ public class Boj_9095 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
+        int[] dp = new int[12];
 
         for (int i = 0; i < T; i++) {
             int current = Integer.parseInt(br.readLine());
-            list.add(current);
-        }
 
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int j = 4; j <= current; j++) {
+                dp[j] = dp[j-1] + dp[j-2] + dp[j-3];
+            }
+            bw.append(String.valueOf(dp[current])).append("\n");
+        }
+        bw.flush();
+        bw.close();
 
     }
 
