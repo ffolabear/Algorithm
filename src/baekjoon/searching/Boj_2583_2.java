112fe75abbd4e563fc0aca7dd73c2899b66c1a9e
ffolabear <ffolabear@gmail.com>
2022-09-28 22:36:55 +0900

    백준 2583 - 영역구하기 푸는중

--- src/baekjoon/searching/Boj_2583_2.java
+++ src/baekjoon/searching/Boj_2583_2.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Boj_2583 {
+public class Boj_2583_2 {
 
     static int N, M, K;
     static int[] dx = {1, -1, 0, 0};
@@ -42,54 +42,19 @@ public class Boj_2583 {
 
         }
 
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 0) {
-                    cnt = 0;
-                    dfs(i, j);
-                    list.add(cnt);
-                }
-            }
-        }
 
-        Collections.sort(list);
-
-        bw.append(String.valueOf(list.size())).append('\n');
-
-        for (int i : list) {
-            bw.append(String.valueOf(i)).append(' ');
-        }
-
-        bw.flush();
-        bw.close();
     }
 
     private static void makingArea(int x1, int y1, int x2, int y2) {
-
-        for (int i = y1; i < y2; i++) {
+        for (int i = 0; i < y2; i++) {
             for (int j = x1; j < x2; j++) {
-                map[j][i] = 1;
+                map[i][j] = 1;
             }
-
         }
 
     }
 
     private static void dfs(int x, int y) {
-
-        map[x][y] = -1;
-        cnt++;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && ny >= 0 && nx < M && ny < N) {
-                if (map[nx][ny] == 0) {
-                    dfs(nx, ny);
-                }
-            }
-        }
 
 
     }
