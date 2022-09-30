c41c9e19308445d9eb4831c9b5662206656808dd
ffolabear <ffolabear@gmail.com>
2022-09-30 23:05:54 +0900

    프로그래머스 lv2 - 캐시 푸는중

---
+++ src/Programmers/level02_1/Cache.java
@@ -0,0 +1,19 @@
+package Programmers.level02_1;
+
+public class Cache {
+
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Cache sol = new Cache();
+        int n = 3;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        sol.solution(n, cities);
+
+    }
+
+}
