95cfec0b30e2f39e909e7adfad859f72aef50cd5
ffolabear <ffolabear@gmail.com>
2022-10-02 23:58:21 +0900

    프로그래머스 lv2 - 튜플 푸는중

---
+++ src/Programmers/level02_1/Tuple2.java
@@ -0,0 +1,18 @@
+package Programmers.level02_1;
+
+public class Tuple2 {
+
+    public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Tuple2 sol = new Tuple2();
+        String s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
+        sol.solution(s);
+
+    }
+
+}
