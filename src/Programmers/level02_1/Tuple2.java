733b7c4b24c61344ff36b91cb64fc63026ddf33d
ffolabear <ffolabear@gmail.com>
2022-10-03 23:00:36 +0900

    프로그래머스 lv2 - 튜플 푸는중

--- src/Programmers/level02_1/Tuple2.java
+++ src/Programmers/level02_1/Tuple2.java
@@ -2,8 +2,38 @@ package Programmers.level02_1;
 
 public class Tuple2 {
 
-    public int[] solution(String s) {
-        int[] answer = {};
+    public String[] solution(String s) {
+
+        String[] answer = {};
+
+        String regex = "[{]";
+        String covt = s.replaceAll(regex, "");
+        String[] nums = covt.split("}");
+
+        String[][] strArr = new String[nums.length][];
+
+        int maxLength = nums[0].length();
+
+
+        for (int i = 0; i < nums.length; i++) {
+            maxLength = Math.max(maxLength, nums[i].length());
+        }
+
+        String longest = "";
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i].length() == maxLength) {
+                longest = nums[i];
+
+            }
+        }
+
+        longest = longest.substring(1, longest.length());
+        System.out.println(longest);
+
+        answer = longest.split(",");
+
+
         return answer;
     }
 
