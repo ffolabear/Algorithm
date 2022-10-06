f2d69793e8853c200c9910c98d8b7b7abd8b7693
ffolabear <ffolabear@gmail.com>
2022-10-06 23:42:22 +0900

    백준 브루트포스 1269 - 대칭 차집합

--- src/baekjoon/map/Boj_1269.java
+++ src/baekjoon/map/Boj_1269.java
@@ -1,2 +1,45 @@
-package baekjoon.map;public class Boj_1269 {
+package baekjoon.map;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Boj_1269 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        HashSet<Integer> set = new HashSet<>();
+
+        int[] A = new int[n];
+        int[] B = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            set.add(A[i]);
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+            if (set.contains(B[i])) {
+                set.remove(B[i]);
+            } else {
+                set.add(B[i]);
+            }
+        }
+
+        System.out.println(set.size());
+
+    }
+
 }
