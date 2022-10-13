9c2b85f5d4a8e6a24fe10e26386eecb59680ec67
ffolabear <ffolabear@gmail.com>
2022-10-13 22:47:54 +0900

    백준 정렬 10825 - 국영수 푸는중

---
+++ src/baekjoon/sort/Boj_10825.java
@@ -0,0 +1,17 @@
+package baekjoon.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_10825 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+    }
+
+}
