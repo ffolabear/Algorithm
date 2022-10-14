5f90e1b554862e732df7ab6ca27ca3f8c8e4afef
ffolabear <ffolabear@gmail.com>
2022-10-14 17:47:43 +0900

    백준 정렬 10825 - 국영수 풀었음

--- src/baekjoon/sort/Boj_10825.java
+++ src/baekjoon/sort/Boj_10825.java
@@ -46,29 +46,20 @@ public class Boj_10825 {
 
             @Override
             public int compare(Info o1, Info o2) {
-                if (o1.kor == o2.kor) {
-                    if (o1.eng == o2.eng) {
-                        if (o1.math == o2.math) {
-                            int len = Math.min(o1.name.length(), o2.name.length());
-                            for (int i = 0; i < len; i++) {
-                                String o1_str = o1.name.toLowerCase(Locale.ROOT);
-                                String o2_str = o2.name.toLowerCase(Locale.ROOT);
 
-                                if (o1_str.charAt(i) != o2_str.charAt(i)) {
-                                    return o1.name.toLowerCase(Locale.ROOT).charAt(i) - o2.name.toLowerCase(Locale.ROOT).charAt(i);
-                                }
-                            }
-                            return o1.name.length() - o2.name.length();
-
-                        } else {
-                            return o2.math - o1.math;
-                        }
-
-                    } else {
-                        return o1.eng - o2.eng;
-                    }
+                if (o1.kor != o2.kor) {
+                    return o2.kor - o1.kor;
                 }
-                return o2.kor - o1.kor;
+
+                if (o1.eng != o2.eng) {
+                    return o1.eng - o2.eng;
+                }
+
+                if (o1.math != o2.math) {
+                    return o2.math - o1.math;
+                }
+
+                return o1.name.compareTo(o2.name);
             }
 
         });
