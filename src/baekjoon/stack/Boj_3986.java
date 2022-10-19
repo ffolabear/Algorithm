845b0dd5efa0c806dc6b059adb8e3e5edd452f4c
ffolabear <ffolabear@gmail.com>
2022-10-20 07:38:58 +0900

    백준 자료구조 3986 - 좋은 단어 푸는중

---
+++ src/baekjoon/stack/Boj_3986.java
@@ -0,0 +1,20 @@
+package baekjoon.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_3986 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+        }
+
+
+    }
+
+}
