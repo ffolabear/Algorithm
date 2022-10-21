741b98e2d33532df743bc84e926df4446bd6eb57
ffolabear <ffolabear@gmail.com>
2022-10-21 22:48:24 +0900

    백준 자료구조 3986 - 좋은 단어 푸는중

--- src/baekjoon/stack/Boj_3986.java
+++ src/baekjoon/stack/Boj_3986.java
@@ -6,14 +6,40 @@ import java.io.InputStreamReader;
 
 public class Boj_3986 {
 
+    static int cnt = 0;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
+
+            function(input);
         }
 
+
+    }
+
+    private static void function(String input) {
+
+//        boolean a = false;
+//        boolean b = false;
+//        char init = ' ';
+
+        boolean a = false;
+        boolean b = false;
+        char init = input.charAt(0);
+
+        if (input.charAt(0) == 'A') {
+            a = true;
+        } else {
+            b = true;
+        }
+
+        for (int i = 1; i < input.length(); i++) {
+        }
 
     }
 
