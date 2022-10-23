85c11ff0b43cb69938a7e29ea838dcfd2993fa02
ffolabear <ffolabear@gmail.com>
2022-10-23 23:59:13 +0900

    프로그래머스 lv2 - 할인행사 푸는중

---
+++ src/Programmers/level02_1/DiscountEvent.java
@@ -0,0 +1,35 @@
+package Programmers.level02_1;
+
+import java.util.HashMap;
+
+public class DiscountEvent {
+
+    public int solution(String[] want, int[] number, String[] discount) {
+
+        int answer = 0;
+
+        //1 ≤ want의 길이 = number의 길이 ≤ 10
+        //10 ≤ discount의 길이 ≤ 100,000
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < want.length; i++) {
+            map.put(want[i], number[i]);
+        }
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        DiscountEvent sol = new DiscountEvent();
+
+        String[] want = {"banana", "apple", "rice", "pork", "pot"};
+        int[] numbers = {3, 2, 2, 2, 1};
+        String[] discount = {"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"};
+        sol.solution(want, numbers, discount);
+
+    }
+}
