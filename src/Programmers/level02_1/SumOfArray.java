5da353cddc98a03d52d0c7c216c3c5ed78f6cdcf
ffolabear <ffolabear@gmail.com>
2022-10-25 22:23:08 +0900

    프로그래머스 lv2 - 연속 부분 수열 합의 개수

--- src/Programmers/level02_1/SumOfArray.java
+++ src/Programmers/level02_1/SumOfArray.java
@@ -16,6 +16,9 @@ public class SumOfArray {
 
             //nCr = n!/r! * (n-r)!
             //길이가 r 인 조합을 계속 만들기?
+            //부분수열의 합을 set 에 저장해서 중복체크하기
+            //원형 수열을 어떻게 구현할지?
+
         }
 
         return answer;
