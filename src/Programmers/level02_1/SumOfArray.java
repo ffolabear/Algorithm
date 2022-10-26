28a466682c544738adfa5e221807c540899b489c
ffolabear <ffolabear@gmail.com>
2022-10-26 22:48:17 +0900

    프로그래머스 lv2 - 연속 부분 수열 합의 개수 푸는중 원형수열부분 구현 미완성

--- src/Programmers/level02_1/SumOfArray.java
+++ src/Programmers/level02_1/SumOfArray.java
@@ -19,8 +19,22 @@ public class SumOfArray {
             //부분수열의 합을 set 에 저장해서 중복체크하기
             //원형 수열을 어떻게 구현할지?
 
+            //길이 0, 1, 2, ...
+            for (int j = 0; j < i; j++) {
+                //0 ~ end 까지
+                int sum = 0;
+                for (int k = start; k < end; k++) {
+                    sum += elements[k];
+                }
+                start++;
+//                System.out.println(sum);
+                set.add(sum);
+            }
+
+
         }
 
+        System.out.println(set);
         return answer;
     }
 
